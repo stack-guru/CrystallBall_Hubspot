f3b0c0b3d06f8ff4537b9728142cd647b02523ec
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2020-10-23 12:36:56 +0500

    Adding is_enabled column

--- app/Models/Annotation.php
+++ app/Models/Annotation.php
@@ -9,6 +9,7 @@ class Annotation extends Model
     protected $fillable = [
         'user_id', 'category', 'event_type', 'event_name',
         'url', 'description', 'title', 'show_at', 'type',
+        'is_enabled'
     ];
 
     public function user()
