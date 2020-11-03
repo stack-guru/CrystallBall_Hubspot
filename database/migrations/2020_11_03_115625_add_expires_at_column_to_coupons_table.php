1402d1aa86b579977c3a57369ed03e6c78dfeb0a
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2020-11-03 17:03:28 +0500

    Adding coupon column to db

---
+++ database/migrations/2020_11_03_115625_add_expires_at_column_to_coupons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddExpiresAtColumnToCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->date('expires_at')->required();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->dropColumn('expires_at');
+        });
+    }
+}
