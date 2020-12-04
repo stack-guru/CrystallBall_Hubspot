d2ac05acfa7671cbf68f5511891509cc9fb5862b
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2020-12-04 15:49:58 +0500

    Added Data Source for users table

---
+++ app/Http/Controllers/UserDataSourceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserDataSource;
+use Illuminate\Http\Request;
+
+class UserDataSourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserDataSource  $userDataSource
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserDataSource $userDataSource)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserDataSource  $userDataSource
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserDataSource $userDataSource)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserDataSource  $userDataSource
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserDataSource $userDataSource)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserDataSource  $userDataSource
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserDataSource $userDataSource)
+    {
+        //
+    }
+}
