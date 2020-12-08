9152339da66c1812dc2578266c572234c641ec9e
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2020-12-08 16:34:40 +0500

    Setting up integrations page

--- resources/js/react/UserInterface/components/integrations/IntegrationsIndex.js
+++ resources/js/react/UserInterface/components/integrations/IntegrationsIndex.js
@@ -13,7 +13,7 @@ export default class integrationsIndex extends React.Component{
 
     render() {
         return (
-            <div className="container-xl bg-white  d-flex flex-column justify-content-center component-wrapper">
+            <div className="container-xl bg-white  d-flex flex-column justify-content-center component-wrapper data-source-container">
 
                 <div className="row ml-0 mr-0">
                     <div className="col-12">
@@ -27,47 +27,47 @@ export default class integrationsIndex extends React.Component{
                                 <img src="/images/mailchamp.jpeg" className="img-fluid int-img" alt="mailchamp image here"/>
                             </figure>
                             <div className="int-description">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus molestias officia, pers
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus molestias officia, pers
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus molestias officia, pers
-                                </p>
+                                <p>Add an annotation to GA automatically when a MailChimp campaign is sent.</p>
+                                <a target="_blank" href="https://www.youtube.com/channel/UCYJIFsw2yAqJybQn3HKYSxw"><i className="fa fa-play"></i> See how this automations works</a>
                             </div>
                             <div className="int-action">
-                                <a href="" className="btn btn-primary">See instructions</a>
+                                <a target="_blank" href="https://www.gaannotations.com/post/add-an-annotation-to-ga-automatically-when-a-mailchimp-campaign-is-sent" className="btn btn-primary">See instructions</a>
                             </div>
-
                         </div>
                         <div className="d-flex flex-row integration-pack">
                             <figure>
                                 <img src="/images/adword.png" className="img-fluid int-img" alt="adword image here"/>
                             </figure>
                             <div className="int-description">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus molestias officia, pers</p>
+                                <p>Add an annotation to GA automatically when a New Google Ads campaign Launch.</p>
+                                <a target="_blank" href="https://www.youtube.com/channel/UCYJIFsw2yAqJybQn3HKYSxw"><i className="fa fa-play"></i> See how this automations works</a>
                             </div>
                             <div className="int-action">
-                                <a href="" className="btn btn-primary">See instructions</a>
+                                <a target="_blank" href="https://www.gaannotations.com/post/add-an-annotation-to-ga-automatically-when-a-launch-a-new-google-ads-campaign" className="btn btn-primary">See instructions</a>
                             </div>
                         </div>
                         <div className="d-flex flex-row integration-pack">
                             <figure>
-                                <img src="/images/asana.png" className="img-fluid int-img" alt="adword image here"/>
+                                <img src="/images/asana.png" className="img-fluid int-img" alt="asana image here"/>
                             </figure>
                             <div className="int-description">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus molestias officia, pers</p>
+                                <p>Add an annotation to GA automatically when a New Message Posted to Private Channel in Asana.</p>
+                                <a target="_blank" href="https://www.youtube.com/channel/UCYJIFsw2yAqJybQn3HKYSxw"><i className="fa fa-play"></i> See how this automations works</a>
                             </div>
                             <div className="int-action">
-                                <a href="" className="btn btn-primary">See instructions</a>
+                                <a target="_blank" href="https://www.gaannotations.com/post/add-an-annotation-to-ga-automatically-when-a-new-message-posted-to-a-private-channel-in-asana" className="btn btn-primary">See instructions</a>
                             </div>
                         </div>
                         <div className="d-flex flex-row integration-pack">
                             <figure >
-                                <img src="/images/slack.png" className="img-fluid int-img" alt="adword image here"/>
+                                <img src="/images/slack.png" className="img-fluid int-img" alt="slack image here"/>
                             </figure>
                             <div className="int-description">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus molestias officia, pers</p>
+                                <p>Add an annotation to GA automatically when a New Message Posted to Private Channel in Slack</p>
+                                <a target="_blank" href="https://www.youtube.com/channel/UCYJIFsw2yAqJybQn3HKYSxw"><i className="fa fa-play"></i> See how this automations works</a>
                             </div>
                             <div className="int-action">
-                                <a href="" className="btn btn-primary">See instructions</a>
+                                <a target="_blank" href="https://www.gaannotations.com/post/add-an-annotation-to-ga-automatically-when-a-new-message-posted-to-a-private-channel-in-slack" className="btn btn-primary">See instructions</a>
                             </div>
                         </div>
                     </div>
