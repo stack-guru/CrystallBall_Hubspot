70245ec73c64f6c0af00783f248beb9ca868b857
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2020-12-31 15:29:48 +0500

    Fixed notice showing box

--- resources/views/helpers/messages.blade.php
+++ resources/views/helpers/messages.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
 <div class="row">
     <div class="col-12">
-      <div class="callout callout-danger">
+      <div class="alert alert-danger">
         <h5><i class="fas fa-times"></i> Error(s):</h5>
         <ul>
             @foreach ($errors->all() as $error)
@@ -16,7 +16,7 @@
 @if(session('success'))
 <div class="row">
     <div class="col-12">
-      <div class="callout callout-success">
+      <div class="alert alert-success">
         <h5><i class="fa fa-check"></i> Done!</h5>
       </div>
     </div>
@@ -26,7 +26,7 @@
 @if(session('error'))
 <div class="row">
     <div class="col-12">
-      <div class="callout callout-danger">
+      <div class="alert alert-danger">
         <h5><i class="fas fa-times"></i> Error!</h5>
         <p>{{session('error')}}</p>
       </div>
