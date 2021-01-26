28ad086ee236a8129c6d91aeb00eafa627130dd7
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-01-26 13:45:47 +0500

    Extract last name from full name in SendGrid service

--- app/Services/SendGridService.php
+++ app/Services/SendGridService.php
@@ -20,14 +20,17 @@ class SendGridService
     {
 
         // Here explode function is used to separate first_name of user from full name. It is not the recommended way to do it but it will keep things simple.
+        $nameChunks = explode(' ', trim($user->name));
+        $firstName = $nameChunks[0];
+        $lastName = $nameChunks[count($nameChunks)-1] != $firstName ? $nameChunks[count($nameChunks)-1] : '';
         $response = Http::withToken($this->key)
             ->withHeaders([
                 "Content-Type: application/json",
             ])
-            ->withBody(json_encode([['email' => $user->email, 'first_name' => explode(' ', trim($user->name))[0], 'e9_D' => $user->created_at->subDays(2)->format('Y-m-d')]]), 'application/json')
+            ->withBody(json_encode([['email' => $user->email, 'first_name' => $firstName, 'last_name' => $lastName, 'e9_D' => $user->created_at->subDays(2)->format('Y-m-d')]]), 'application/json')
             ->post("https://api.sendgrid.com/v3/contactdb/recipients");
 
-        Log::channel('sendgrid')->info('Adding recipient to SendGrid Contact DB.', ['email' => $user->email, 'first_name' => explode(' ', trim($user->name))[0], 'e9_D' => $user->created_at->subDays(2)->format('Y-m-d')]);
+        Log::channel('sendgrid')->info('Adding recipient to SendGrid Contact DB.', ['email' => $user->email, 'first_name' => $firstName, 'last_name' => $lastName, 'e9_D' => $user->created_at->subDays(2)->format('Y-m-d')]);
         Log::channel('sendgrid')->debug($response->body());
 
         if ($response->status() != 201) {
@@ -46,6 +49,10 @@ class SendGridService
             return false;
         }
 
+        $nameChunks = explode(' ', trim($user->name));
+        $firstName = $nameChunks[0];
+        $lastName = $nameChunks[count($nameChunks)-1] != $firstName ? $nameChunks[count($nameChunks)-1] : '';
+
         $response = Http::withToken($this->key)
             ->withHeaders([
                 "Content-Type: application/json",
@@ -53,12 +60,12 @@ class SendGridService
             ->withBody(json_encode([
                 "list_ids" => [$list['id']],
                 "contacts" => [
-                    ['email' => $user->email, 'first_name' => explode(' ', trim($user->name))[0], 'custom_fields' => ['e9_D' => $user->created_at->subDays(2)->format('Y-m-d')]],
+                    ['email' => $user->email, 'first_name' => $firstName, 'last_name' => $lastName, 'custom_fields' => ['e9_D' => $user->created_at->subDays(2)->format('Y-m-d')]],
                 ],
             ]), 'application/json')
             ->put("https://api.sendgrid.com/v3/marketing/contacts");
 
-        Log::channel('sendgrid')->info('Adding user to a list:' . $listName, ['email' => $user->email, 'first_name' => explode(' ', trim($user->name))[0], 'custom_fields' => ['e9_D' => $user->created_at->subDays(2)->format('Y-m-d')], $list['id']]);
+        Log::channel('sendgrid')->info('Adding user to a list:' . $listName, ['email' => $user->email, 'first_name' => $firstName, 'last_name' => $lastName, 'custom_fields' => ['e9_D' => $user->created_at->subDays(2)->format('Y-m-d')], $list['id']]);
         Log::channel('sendgrid')->debug($response->body());
 
         if ($response->status() != 202) {
@@ -77,9 +84,13 @@ class SendGridService
         }
 
         $contactsDbArray = array_map(function ($user) {
+            $nameChunks = explode(' ', trim($user['name']));
+            $firstName = $nameChunks[0];
+            $lastName = $nameChunks[count($nameChunks)-1] != $firstName ? $nameChunks[count($nameChunks)-1] : '';
             return [
                 'email' => $user['email'],
-                'first_name' => explode(' ', trim($user['name']))[0],
+                'first_name' => $firstName,
+                'last_name' => $lastName,
                 // 'custom_fields' => ['e9_D' => Carbon::parse($user['created_at'])->subDays(2)->format('Y-m-d')],
             ];
         }, $users);
