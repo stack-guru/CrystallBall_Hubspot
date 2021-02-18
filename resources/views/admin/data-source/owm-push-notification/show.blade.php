03fa50dee0b070467fa615cc7ae1e9a008272f90
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-02-18 17:06:14 +0500

    Hotfix

--- resources/views/admin/data-source/owm-push-notification/show.blade.php
+++ resources/views/admin/data-source/owm-push-notification/show.blade.php
@@ -6,13 +6,13 @@
     <div class="row ml-0 mr-0 justify-content-center">
         <div class="col-md-10 p-5">
             <h1 class="my-4 ">Open Weather Map Push Notification</h1>
-            @foreach($oWMPushNotification->toArray() as $key => $value)
+
+            @foreach($oWMPushNotification as $key => $value)
                 <h4><strong>{{$key}}</strong> {{ $value }}</h4>
             @endforeach
         </div>
     </div>
 
-</div>
 </div>
 
 @endsection
