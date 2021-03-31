a3242b75df0fc9e352d1ab735fd684aa7e9a0e46
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-03-31 19:24:37 +0500

    Shown annotations from google alerts in extension api

--- app/Http/Controllers/API/AnnotationController.php
+++ app/Http/Controllers/API/AnnotationController.php
@@ -139,7 +139,11 @@ class AnnotationController extends Controller
             $annotationsQuery .= "select alert_date, open_weather_map_alerts.id, open_weather_map_cities.name, description, null, description from `open_weather_map_alerts` inner join `user_data_sources` as `uds` on `uds`.`open_weather_map_city_id` = `open_weather_map_alerts`.open_weather_map_city_id inner join `user_data_sources` as `owmes` on `owmes`.`open_weather_map_event` = `open_weather_map_alerts`.`event` inner join `open_weather_map_cities` on `open_weather_map_cities`.id = `open_weather_map_alerts`.`open_weather_map_city_id` where `uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'open_weather_map_cities'";
         }
         ////////////////////////////////////////////////////////////////////
-
+        if ($user->is_ds_google_alerts_enabled) {
+            $annotationsQuery .= " union ";
+            $annotationsQuery .= "select 1, alert_date, alert_date as created_at, null, \"Google Alert\", title, url, description, 'System' AS user_name from `google_alerts` inner join `user_data_sources` as `uds` on `uds`.`value` = `google_alerts`.tag_name where `uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'google_alert_keywords'";
+        }
+        ////////////////////////////////////////////////////////////////////
         $annotationsQuery .= ") AS TempTable WHERE DATE(`show_at`) BETWEEN '" . $startDate->format('Y-m-d') . "' AND '" . $endDate->format('Y-m-d') . "' ORDER BY show_at ASC";
         $annotations = DB::select($annotationsQuery);
 
