63a320d3e53f1e77363a635c2f5f4892b235618e
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-04-05 13:24:01 +0500

    Added Google Analytics code to login and register pages

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -29,6 +29,16 @@
 
     </script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.analytics.code')}}"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    
+    gtag('config', '{{ config('services.google.analytics.code')}}');
+    </script>
+    
 <script type="text/javascript">
   (function(c,l,a,r,i,t,y){
   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
