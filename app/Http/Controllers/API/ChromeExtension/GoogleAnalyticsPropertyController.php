35fca57a391b2e837246659791caea06bdbccc94
Muneeb Amjad <muneebamjad302@gmail.com>
2021-04-21 18:16:12 +0500

    Added not filter Array before Showing All GoogleAnalyticsproperties

--- app/Http/Controllers/API/ChromeExtension/GoogleAnalyticsPropertyController.php
+++ app/Http/Controllers/API/ChromeExtension/GoogleAnalyticsPropertyController.php
@@ -15,17 +15,17 @@ class GoogleAnalyticsPropertyController extends Controller
 {
     public function index(Request $request)
     {
-        $properties=GoogleAnalyticsProperty::where('user_id',Auth::id())->orderBy('name')->with('googleAccount:id,name')->get(['id','name','google_account_id']);
-        return $properties;
-        // return ['google_analytics_accounts' => array_merge(
-        //     [
-        //         ['id' => '*', 'name' => 'No Filter'],
-        //     ],
-        //     GoogleAnalyticsProperty::select('google_analytics_accounts.id', 'name')->ofCurrentUser()
-        //         ->distinct()
-        //         ->join('annotation_ga_accounts', 'google_analytics_accounts.id', 'annotation_ga_accounts.google_analytics_account_id')
-        //         ->get()->toArray()
-        // )];
+        // $properties=GoogleAnalyticsProperty::where('user_id',Auth::id())->orderBy('name')->with('googleAccount:id,name')->get(['id', 'name', 'google_account_id']);
+        // return $properties;
+        return ['google_analytics_accounts' => array_merge(
+            [
+                ['id' => '*', 'name' => 'No Filter',  "google_account"=>[
+                    "id"=> null,
+                    "name"=> null
+                ]],
+            ],
+            GoogleAnalyticsProperty::where('user_id',Auth::id())->orderBy('name')->with('googleAccount:id,name')->get(['id', 'name', 'google_account_id'])->toArray()
+        )];
 
     }
     
