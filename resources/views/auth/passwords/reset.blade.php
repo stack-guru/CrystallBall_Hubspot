e97fdbb1350ef17930ec50257a7f098b9c95ab53
Muneeb Amjad <muneebamjad302@gmail.com>
2021-04-28 12:56:52 +0500

    Removed the email input field from the resset passord ppage

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -15,19 +15,19 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group row">
+                        {{-- <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" readonly value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-6"> --}}
+                                <input id="email" type="hidden" class="form-control @error('email') is-invalid @enderror" name="email" readonly value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
+                                {{-- @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> --}}
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
