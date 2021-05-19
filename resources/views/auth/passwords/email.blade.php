92773d47a7b0a4dc524984a6c7d33aa30853b9b1
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-05-19 14:15:32 +0500

    Reformatted reset password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,47 +4,28 @@
 @section('meta-description', 'Reset your password to GAannotations')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><h1>{{ __('Reset Password') }}</h1></div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<form class="form-signin" method="POST" action="{{ route('password.email') }}">
+    @csrf
+    <div class="text-center mb-4">
+      <h1 class="h3 mb-3 font-weight-normal">Reset Password</h1>
+      {{--        <p>Google Analytics Annotations Amplified</p>--}}
     </div>
-</div>
+    @if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+    @endif
+    <div class="form-label-group">
+      <input type="email" id="inputEmail" class="form-control @error('email') is-invalid @enderror"
+        placeholder="Email address" required="" autofocus="" name="email" value="{{ old('email') }}">
+      <label for="inputEmail">Email address</label>
+      @error('email')
+      <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+      </span>
+      @enderror
+    </div>
+  
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Send Password Reset Link</button>
+  </form>
 @endsection
