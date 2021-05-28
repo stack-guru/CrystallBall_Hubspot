5fbfa82aa702f9e4ab4756010caf1d31d04bc734
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-05-28 13:06:12 +0500

    Hotfix for news alert delay

--- app/Http/Controllers/AnnotationController.php
+++ app/Http/Controllers/AnnotationController.php
@@ -242,7 +242,7 @@ class AnnotationController extends Controller
         }
         if ($user->is_ds_google_alerts_enabled) {
             $annotationsQuery .= " union ";
-            $annotationsQuery .= "select 1, alert_date, alert_date as created_at, null, \"News Alert\", title, url, description, 'System' AS user_name from `google_alerts` inner join `user_data_sources` as `uds` on `uds`.`value` = `google_alerts`.tag_name where $gAPropertyCriteria AND (`uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'google_alert_keywords' AND DATE(google_alerts.created_at) > DATE(uds.created_at))";
+            $annotationsQuery .= "select 1, alert_date, alert_date as created_at, null, \"News Alert\", title, url, description, 'System' AS user_name from `google_alerts` inner join `user_data_sources` as `uds` on `uds`.`value` = `google_alerts`.tag_name where $gAPropertyCriteria AND (`uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'google_alert_keywords' AND DATE(google_alerts.created_at) > (DATE(uds.created_at)+1))";
         }
         if ($user->is_ds_wordpress_updates_enabled) {
             $annotationsQuery .= " union ";
