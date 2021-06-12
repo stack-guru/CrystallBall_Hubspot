7f44a9f66161e03b0c4bdb8833dc4163e1730043
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2021-06-12 13:16:13 +0500

    Added new data source filters in chrome extension api

--- app/Http/Controllers/API/ChromeExtension/AnnotationController.php
+++ app/Http/Controllers/API/ChromeExtension/AnnotationController.php
@@ -54,6 +54,17 @@ class AnnotationController extends Controller
             $annotationsQuery .= " AND (`annotation_ga_properties`.`google_analytics_property_id` IS NULL OR `annotation_ga_properties`.`google_analytics_property_id` = " . $gaPropertyId . ")";
             $gAPropertyCriteria = "`uds`.`ga_property_id` = $gaPropertyId";
         }
+
+        if ($request->query('google_analytics_property_id') && $request->query('google_analytics_property_id') !== '*') {
+            $gaPropertyId = $request->query('google_analytics_property_id');
+            $annotationsQuery .= " AND (`annotation_ga_properties`.`google_analytics_property_id` IS NULL OR `annotation_ga_properties`.`google_analytics_property_id` = " . $gaPropertyId . ")";
+            $gAPropertyCriteria = "`uds`.`ga_property_id` = $gaPropertyId";
+        }
+
+        if ($user->is_ds_web_monitors_enabled && $request->query('show_website_monitoring') == 'false') {
+            $annotationsQuery .= " AND annotations.category = 'Website Monitoring')";
+        }
+
         $addedByArray = [];
         if ($request->query('show_manual_annotations')) {
             array_push($addedByArray, 'manual');
@@ -95,12 +106,12 @@ class AnnotationController extends Controller
             $annotationsQuery .= "select alert_date, open_weather_map_alerts.id, open_weather_map_cities.name, description, null, description from `open_weather_map_alerts` inner join `user_data_sources` as `uds` on `uds`.`open_weather_map_city_id` = `open_weather_map_alerts`.open_weather_map_city_id inner join `user_data_sources` as `owmes` on `owmes`.`open_weather_map_event` = `open_weather_map_alerts`.`event` inner join `open_weather_map_cities` on `open_weather_map_cities`.id = `open_weather_map_alerts`.`open_weather_map_city_id` where $gAPropertyCriteria AND (`uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'open_weather_map_cities')";
         }
         ////////////////////////////////////////////////////////////////////
-        if ($user->is_ds_google_alerts_enabled) {
+        if ($user->is_ds_google_alerts_enabled && $request->query('show_news_alerts') == 'true') {
             $annotationsQuery .= " union ";
             $annotationsQuery .= "select alert_date, google_alerts.id, 'News Alerts', title, url, description from `google_alerts` inner join `user_data_sources` as `uds` on `uds`.`value` = `google_alerts`.tag_name where $gAPropertyCriteria AND (`uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'google_alert_keywords' AND DATE(google_alerts.created_at) > DATE(DATE_ADD(uds.created_at, INTERVAL 1 DAY)) )";
         }
         ////////////////////////////////////////////////////////////////////
-        if ($user->is_ds_wordpress_updates_enabled) {
+        if ($user->is_ds_wordpress_updates_enabled && $request->query('show_wordpress_updates') == 'true') {
             $annotationsQuery .= " union ";
             $annotationsQuery .= "select update_date, wordpress_updates.id, category, event_name, url, description from `wordpress_updates`";
             if (UserDataSource::ofCurrentUser()->where('ds_code', 'wordpress_updates')->where('value', 'last year')->count()) {
@@ -258,6 +269,11 @@ class AnnotationController extends Controller
             $annotationsQuery .= " AND (`annotation_ga_properties`.`google_analytics_property_id` IS NULL OR `annotation_ga_properties`.`google_analytics_property_id` = " . $gaPropertyId . ")";
             $gAPropertyCriteria = "`uds`.`ga_property_id` = $gaPropertyId";
         }
+
+        if ($user->is_ds_web_monitors_enabled && $request->query('show_website_monitoring') == 'false') {
+            $annotationsQuery .= " AND annotations.category = 'Website Monitoring')";
+        }
+        
         $addedByArray = [];
         if ($request->query('show_manual_annotations')) {
             array_push($addedByArray, 'manual');
@@ -299,12 +315,12 @@ class AnnotationController extends Controller
             $annotationsQuery .= "select alert_date, open_weather_map_alerts.id, open_weather_map_cities.name, description, null, description from `open_weather_map_alerts` inner join `user_data_sources` as `uds` on `uds`.`open_weather_map_city_id` = `open_weather_map_alerts`.open_weather_map_city_id inner join `user_data_sources` as `owmes` on `owmes`.`open_weather_map_event` = `open_weather_map_alerts`.`event` inner join `open_weather_map_cities` on `open_weather_map_cities`.id = `open_weather_map_alerts`.`open_weather_map_city_id` where $gAPropertyCriteria AND (`uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'open_weather_map_cities')";
         }
         ////////////////////////////////////////////////////////////////////
-        if ($user->is_ds_google_alerts_enabled) {
+        if ($user->is_ds_google_alerts_enabled && $request->query('show_news_alerts') == 'true') {
             $annotationsQuery .= " union ";
             $annotationsQuery .= "select alert_date, google_alerts.id, 'News Alert', title, url, description from `google_alerts` inner join `user_data_sources` as `uds` on `uds`.`value` = `google_alerts`.tag_name where $gAPropertyCriteria AND (`uds`.`user_id` = " . $user->id . " and `uds`.`ds_code` = 'google_alert_keywords' AND DATE(google_alerts.created_at) > DATE(DATE_ADD(uds.created_at, INTERVAL 1 DAY)) )";
         }
         ////////////////////////////////////////////////////////////////////
-        if ($user->is_ds_wordpress_updates_enabled) {
+        if ($user->is_ds_wordpress_updates_enabled && $request->query('show_wordpress_updates') == 'true') {
             $annotationsQuery .= " union ";
             $annotationsQuery .= "select update_date, wordpress_updates.id, category, event_name, url, description from `wordpress_updates`";
             if (UserDataSource::ofCurrentUser()->where('ds_code', 'wordpress_updates')->where('value', 'last year')->count()) {
@@ -313,6 +329,7 @@ class AnnotationController extends Controller
         }
         ////////////////////////////////////////////////////////////////////
         $annotationsQuery .= ") AS TempTable WHERE DATE(`show_at`) BETWEEN '" . $startDate->format('Y-m-d') . "' AND '" . $endDate->format('Y-m-d') . "' ORDER BY show_at ASC";
+
         $annotations = DB::select($annotationsQuery);
 
         return ['annotations' => $annotations];
