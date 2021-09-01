f859e78e6f7548ee2d96ef3ca00c1c72bac164a5
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2021-09-01 15:30:37 +0500

    Hotfix

--- resources/views/admin/auto-payment-logs/index.blade.php
+++ resources/views/admin/auto-payment-logs/index.blade.php
@@ -38,7 +38,7 @@
                                 <td>{{$autoPaymentLog->was_successful}}</td>
                                 <td>{{$autoPaymentLog->created_at}}</td>
                                 <td>
-                                    <a href="{{ route('admin.deduct-payment.create', ['user_id' => $paymentDetail->user_id, 'payment_detail_id' => $paymentDetail->id, 'amount' => $autoPaymentLog->charged_price]) }}" class="btn btn-primary">Charge</a>
+                                    <a href="{{ route('admin.deduct-payment.create', ['user_id' => $autoPaymentLog->user_id, 'payment_detail_id' => $autoPaymentLog->paymentDetail->id, 'amount' => $autoPaymentLog->charged_price]) }}" class="btn btn-primary">Charge</a>
                                 </td>
                             </tr>
                             @empty
