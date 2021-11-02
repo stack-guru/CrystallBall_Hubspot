8b12cbb607aa025cbe7d5a263f966c519dd889c0
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2021-11-02 15:28:54 +0500

    Added fetching job for search console statistics in fetching command

--- app/Console/Commands/FetchGoogleSearchConsoleSitesStatistics.php
+++ app/Console/Commands/FetchGoogleSearchConsoleSitesStatistics.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Models\GoogleSearchConsoleSite;
 use App\Models\GoogleSearchConsoleStatistics;
 use App\Services\GoogleSearchConsoleService;
+use App\Jobs\FetchGSCSStatisticsJob;
 use Carbon\Carbon;
 
 class FetchGoogleSearchConsoleSitesStatistics extends Command
@@ -41,7 +42,6 @@ class FetchGoogleSearchConsoleSitesStatistics extends Command
      */
     public function handle()
     {
-        $gSCS = new GoogleSearchConsoleService;
         if ($this->option('from-past')) {
             $startDate = '2021-01-01';
             $endDate = Carbon::yesterday()->format('Y-m-d');
@@ -51,60 +51,7 @@ class FetchGoogleSearchConsoleSitesStatistics extends Command
 
         $googleSearchConsoleSites = GoogleSearchConsoleSite::with('googleAccount')->get();
         foreach ($googleSearchConsoleSites as $googleSearchConsoleSite) {
-            $this->info("Fetching satistics for $googleSearchConsoleSite->site_url site with permission level $googleSearchConsoleSite->permission_level under account " . $googleSearchConsoleSite->googleAccount->account_id);
-            $dataRows = $gSCS->getGSCStatistics($googleSearchConsoleSite->googleAccount, $googleSearchConsoleSite, $startDate, $endDate);
-            if ($dataRows !== false) {
-                $this->info(count($dataRows) . " rows fetched.");
-                GoogleSearchConsoleStatistics::where('google_search_console_site_id', $googleSearchConsoleSite->id)->whereBetween('statistics_date', [$startDate, $endDate])->delete();
-                $rows = [];
-                foreach ($dataRows as  $dataRow) {
-                    $row = [];
-                    $row['statistics_date'] = $dataRow['values'][4];
-                    $row['query'] = $dataRow['values'][0];
-                    $row['page'] = $dataRow['values'][1];
-                    $row['country'] = $dataRow['values'][2];
-                    $row['device'] = $dataRow['values'][3];
-
-                    $row['clicks_count'] = $dataRow['clicks'];
-                    $row['impressions_count'] = $dataRow['impressions'];
-                    $row['ctr_count'] = $dataRow['ctr'];
-                    $row['position_rank'] = $dataRow['position'];
-
-                    $row['google_search_console_site_id'] = $googleSearchConsoleSite->id;
-                    $row['google_account_id'] = $googleSearchConsoleSite->googleAccount->id;
-                    $row['user_id'] = $googleSearchConsoleSite->user_id;
-                    $rows[] = $row;
-
-                    if (count($rows) > 5000) {
-                        // formula for ^ number is max no. of placeholders in mysql (65535) / no. of columns you have in insert statement (12)
-                        // I obviously rounded it to something human readable
-                        GoogleSearchConsoleStatistics::insert($rows);
-                        $rows = [];
-                    }
-                }
-                if (count($rows)) {
-                    GoogleSearchConsoleStatistics::insert($rows);
-                }
-            }
-
-            $dataRows = $gSCS->getGSCSearchAppearance($googleSearchConsoleSite->googleAccount, $googleSearchConsoleSite, $endDate);
-            if ($dataRows !== false) {
-                $this->info(count($dataRows) . " rows fetched.");
-                foreach ($dataRows as  $dataRow) {
-                    $gSCStatistics = new GoogleSearchConsoleStatistics;
-                    $gSCStatistics->statistics_date = $startDate;
-                    $gSCStatistics->search_appearance = $dataRow['values'][0];
-
-                    $gSCStatistics->clicks_count = $dataRow['clicks'];
-                    $gSCStatistics->impressions_count = $dataRow['impressions'];
-                    $gSCStatistics->ctr_count = $dataRow['ctr'];
-                    $gSCStatistics->position_rank = $dataRow['position'];
-
-                    $gSCStatistics->google_search_console_site_id = $googleSearchConsoleSite->id;
-                    $gSCStatistics->google_account_id = $googleSearchConsoleSite->googleAccount->id;
-                    $gSCStatistics->save();
-                }
-            }
+            FetchGSCSStatisticsJob::dispatch($googleSearchConsoleSite, $startDate, $endDate);
         }
 
         return 0;
--- app/Jobs/FetchGSCSStatisticsJob.php
+++ app/Jobs/FetchGSCSStatisticsJob.php
@@ -8,19 +8,31 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Models\GoogleSearchConsoleSite;
+use App\Models\GoogleSearchConsoleStatistics;
+use App\Services\GoogleSearchConsoleService;
+use Carbon\Carbon;
 
-class FetchGSCSStatisticsJob implements ShouldQueue
+class FetchGSCSStatisticsJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private $googleSearchConsoleSite, $startDate, $endDate;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(GoogleSearchConsoleSite $googleSearchConsoleSite, $startDate, $endDate)
     {
-        //
+        $this->googleSearchConsoleSite = $googleSearchConsoleSite;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+    }
+
+    public function uniqueId()
+    {
+        return $this->googleSearchConsoleSite->id;
     }
 
     /**
@@ -30,6 +42,61 @@ class FetchGSCSStatisticsJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $gSCS = new GoogleSearchConsoleService;
+
+        print ("Fetching satistics for " . $this->googleSearchConsoleSite->site_url . " site with permission level " . $this->googleSearchConsoleSite->permission_level . " under account " . $this->googleSearchConsoleSite->googleAccount->account_id) . "\n";
+        $dataRows = $gSCS->getGSCStatistics($this->googleSearchConsoleSite->googleAccount, $this->googleSearchConsoleSite, $this->startDate, $this->endDate);
+        if ($dataRows !== false) {
+            print (count($dataRows) . " rows fetched.") . "\n";
+            GoogleSearchConsoleStatistics::where('google_search_console_site_id', $this->googleSearchConsoleSite->id)->whereBetween('statistics_date', [$this->startDate, $this->endDate])->delete();
+            $rows = [];
+            foreach ($dataRows as  $dataRow) {
+                $row = [];
+                $row['statistics_date'] = $dataRow['values'][4];
+                $row['query'] = $dataRow['values'][0];
+                $row['page'] = $dataRow['values'][1];
+                $row['country'] = $dataRow['values'][2];
+                $row['device'] = $dataRow['values'][3];
+
+                $row['clicks_count'] = $dataRow['clicks'];
+                $row['impressions_count'] = $dataRow['impressions'];
+                $row['ctr_count'] = $dataRow['ctr'];
+                $row['position_rank'] = $dataRow['position'];
+
+                $row['google_search_console_site_id'] = $this->googleSearchConsoleSite->id;
+                $row['google_account_id'] = $this->googleSearchConsoleSite->googleAccount->id;
+                $row['user_id'] = $this->googleSearchConsoleSite->user_id;
+                $rows[] = $row;
+
+                if (count($rows) > 5000) {
+                    // formula for ^ number is max no. of placeholders in mysql (65535) / no. of columns you have in insert statement (12)
+                    // I obviously rounded it to something human readable
+                    GoogleSearchConsoleStatistics::insert($rows);
+                    $rows = [];
+                }
+            }
+            if (count($rows)) {
+                GoogleSearchConsoleStatistics::insert($rows);
+            }
+        }
+
+        $dataRows = $gSCS->getGSCSearchAppearance($this->googleSearchConsoleSite->googleAccount, $this->googleSearchConsoleSite, $this->endDate);
+        if ($dataRows !== false) {
+            print (count($dataRows) . " rows fetched.") . "\n";
+            foreach ($dataRows as  $dataRow) {
+                $gSCStatistics = new GoogleSearchConsoleStatistics;
+                $gSCStatistics->statistics_date = $this->startDate;
+                $gSCStatistics->search_appearance = $dataRow['values'][0];
+
+                $gSCStatistics->clicks_count = $dataRow['clicks'];
+                $gSCStatistics->impressions_count = $dataRow['impressions'];
+                $gSCStatistics->ctr_count = $dataRow['ctr'];
+                $gSCStatistics->position_rank = $dataRow['position'];
+
+                $gSCStatistics->google_search_console_site_id = $this->googleSearchConsoleSite->id;
+                $gSCStatistics->google_account_id = $this->googleSearchConsoleSite->googleAccount->id;
+                $gSCStatistics->save();
+            }
+        }
     }
 }
