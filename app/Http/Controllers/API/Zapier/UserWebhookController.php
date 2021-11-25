1e2239d02080312c3c89f97b24aa92a2f7f63ad8
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2021-11-25 15:53:20 +0500

    Hotfix for Zapier webhook api

--- app/Http/Controllers/API/Zapier/UserWebhookController.php
+++ app/Http/Controllers/API/Zapier/UserWebhookController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\Zapier;
 
 use App\Http\Requests\UserWebhookRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\UserWebhook;
+use App\Http\Controllers\Controller;
 
 class UserWebhookController extends Controller
 {
