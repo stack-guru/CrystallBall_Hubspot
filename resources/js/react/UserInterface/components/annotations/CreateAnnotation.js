0f5c728a54bdea3ca4a075b737ff40ee4459c733
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-01-20 20:34:46 +0500

    Added popup for limited google analytics property

--- resources/js/react/UserInterface/components/annotations/CreateAnnotation.js
+++ resources/js/react/UserInterface/components/annotations/CreateAnnotation.js
@@ -232,6 +232,7 @@ export default class CreateAnnotation extends React.Component {
                                             multiple
                                             onFocus={(e) => {
                                                 if (this.props.currentPricePlan.ga_account_count == 1) swal("Upgrade to Pro Plan!", "Google Analytics Properties are not available in this plan.", "warning");
+                                                if (this.props.currentPricePlan.google_analytics_property_count == -1) swal("Upgrade your Plan!", "Google Analytics Properties are not available in this plan.", "warning");
                                             }}
                                         />
 
--- resources/js/react/UserInterface/components/annotations/EditAnnotation.js
+++ resources/js/react/UserInterface/components/annotations/EditAnnotation.js
@@ -245,6 +245,7 @@ export default class EditAnnotation extends React.Component {
                                             multiple
                                             onFocus={(e) => {
                                                 if (this.props.currentPricePlan.ga_account_count == 1) swal("Upgrade to Pro Plan!", "Google Analytics Properties are not available in this plan.", "warning");
+                                                if (this.props.currentPricePlan.google_analytics_property_count == -1) swal("Upgrade your Plan!", "Google Analytics Properties are not available in this plan.", "warning");
                                             }}
                                         ></GoogleAnalyticsPropertySelect>
                                     </div>
