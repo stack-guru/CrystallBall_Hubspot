8187847b8c2feda90ecfa1b0b32667e8c3fee7e9
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-01-25 13:33:11 +0500

    Added-back routes for old chrome extension

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'App\Http\Controllers', 'as' => 'api.'], function () {
 
+    // Chrome Extension
+    Route::group(['prefix' => 'v1/chrome-extension', 'as' => 'v1.chrome-extension.'], function () {
+
+        Route::group(['middleware' => ['auth:api']], function () {
+
+            Route::get('user', function (Request $request) {
+                return $request->user();
+            })->name('user.show');
+
+            Route::get('event-sources', 'API\EventSourceController@index');
+            Route::get('annotations', 'API\ChromeExtension\AnnotationController@index');
+            Route::post('annotations', 'API\ChromeExtension\AnnotationController@store');
+            Route::get('annotations/preview', 'API\ChromeExtension\AnnotationController@extensionAnnotationPreview');
+            Route::get('google-accounts', 'API\GoogleAccountController@extensionIndex');
+            Route::get('google-analytics-accounts', 'API\GoogleAnalyticsAccountController@extensionIndex');
+            Route::get('google-analytics-properties', 'API\ChromeExtension\GoogleAnalyticsPropertyController@index');
+            Route::get('google-annotation/{id}', 'API\ChromeExtension\GoogleAnalyticsPropertyController@getAnnotations');
+            Route::get('memberships', 'API\UserController@extensionShowMembership');
+            Route::get('users', 'API\ChromeExtension\UserController@index');
+
+            Route::post('log', 'API\ChromeExtension\ChromeExtensionLogController@store');
+        });
+    });
+
     Route::post('login', 'API\LoginController@login')->name('login')->middleware('cors');
     Route::post('login/google', 'API\LoginController@loginWithGoogle')->name('login.google')->middleware('cors');
     Route::post('logout', 'API\LoginController@logout')->name('logout')->middleware('cors');
