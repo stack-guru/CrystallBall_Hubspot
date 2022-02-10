bb659c42cd6f3132ed6a7858ce20047bf15964ac
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-02-10 16:25:12 +0500

    Fixed top notice bar space on scroll issue

--- resources/js/react/UserInterface/utils/TopNoticeBar.js
+++ resources/js/react/UserInterface/utils/TopNoticeBar.js
@@ -4,6 +4,11 @@ export default function TopNoticeBar(props) {
     if (!props.show) return null;
 
     return (
-        <div className="notice-top-bar" style={{ width: '100%', backgroundImage: props.backgroundColor, textAlign: 'center', padding: '5px' }}>{props.content}</div>
+        <div className="notice-top-bar"
+            style={{
+                width: '100%', backgroundImage: props.backgroundColor, textAlign: 'center', padding: '5px',
+                top: '0px', left: '0px', position: 'sticky',
+            }}
+        >{props.content}</div>
     )
 }
