ac3a4b8648d38d5b5c2ed00db0973e017db0b3c0
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-03-03 12:24:57 +0500

    Revert "Revert "Added support for separate React development""
    This reverts commit b0b802ef59c8b94a4534fef68a4568884485b8e0.

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::group(['prefix' => 'spectator', 'as' => 'spectator.'], function () {
 });
 
 Route::redirect('/', '/login', 301);
+Route::post('ui/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
 
 Auth::routes(['verify' => true]);
 Route::get('register_chrome', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm']);
@@ -49,7 +50,7 @@ Route::group(['prefix' => 'app-sumo', 'as' => 'app-sumo.', 'middleware' => ['aut
     Route::put('password', [App\Http\Controllers\AppSumo\AuthController::class, 'updatePassword'])->name('password.update');
 });
 
-Route::group(['middleware' => ['only.non.empty.password', 'auth']], function () {
+Route::group(['middleware' => ['only.non.empty.password', 'auth:api']], function () {
 
     Route::view('dashboard', 'ui/app'); // obsolete
     Route::view('analytics', 'ui/app');
