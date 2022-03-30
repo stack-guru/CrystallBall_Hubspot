62d8893a42c80f80300228634c35c88b178eeed2
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-03-30 13:43:15 +0500

    Changed Google Analytics Properties response for Zapier API

--- app/Http/Controllers/API/Zapier/MiscellaneousController.php
+++ app/Http/Controllers/API/Zapier/MiscellaneousController.php
@@ -25,7 +25,9 @@ class MiscellaneousController extends Controller
         $resp = [];
         foreach ($googleAnalyticsProperties as $gAP) {
             array_push($resp, [
-                $gAP->id => $gAP->name . " - " . $gAP->GoogleAnalyticsAccount->name . " - " . $gAP->GoogleAccount->name
+                'sample' => '###',
+                'value' => $gAP->id,
+                'label' => $gAP->name . " - " . $gAP->GoogleAnalyticsAccount->name . " - " . $gAP->GoogleAccount->name
             ]);
         }
 
