dfb4a98d14f31fc3f8382e0cd52274d31fd85d99
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-03-30 13:09:35 +0500

    Added API for dynamic field in Zapier

---
+++ app/Http/Controllers/API/Zapier/MiscellaneousController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API\Zapier;
+
+use App\Http\Controllers\Controller;
+use App\Models\GoogleAnalyticsProperty;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MiscellaneousController extends Controller
+{
+
+    public function getGoogleAnalyticsProperty(Request $request)
+    {
+        if (!count(Auth::user()->googleAccounts)) {
+            return [];
+        }
+
+        $googleAnalyticsPropertiesQuery = GoogleAnalyticsProperty::ofCurrentUser()->with(['googleAccount:id,name', 'GoogleAnalyticsAccount:id,name'])->orderBy('name');
+
+        $googleAnalyticsProperties =  $googleAnalyticsPropertiesQuery->get(['id', 'name', 'google_analytics_account_id', 'google_account_id']);
+
+        $resp = [];
+        foreach ($googleAnalyticsProperties as $gAP) {
+            array_push($resp, [
+                'key' => $gAP->id,
+                'label' => $gAP->name . " - " . $gAP->GoogleAnalyticsAccount->name . " - " . $gAP->GoogleAccount->name
+            ]);
+        }
+
+        return $resp;
+    }
+}
