ab31c819bdca1359ca460e2355c0bb626df7a6c9
= <=>
2022-03-31 11:32:27 +0500

    Hide Analytics Page

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::group(['prefix' => 'app-sumo', 'as' => 'app-sumo.', 'middleware' => ['aut
 Route::group(['middleware' => ['only.non.empty.password', 'auth']], function () {
 
     Route::view('dashboard', 'ui/app'); // obsolete
-    Route::view('analytics', 'ui/app');
+    // Route::view('analytics', 'ui/app');
     Route::view('dashboard/analytics', 'ui/app');
     Route::view('dashboard/search-console', 'ui/app');
 
