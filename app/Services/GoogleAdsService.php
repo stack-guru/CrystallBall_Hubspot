6895942075d907959603d835177eb0ef331aa795
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-04-20 15:39:05 +0500

    Adding Google Ads REST APIs

--- app/Services/GoogleAdsService.php
+++ app/Services/GoogleAdsService.php
@@ -20,6 +20,50 @@ class GoogleAdsService  extends GoogleAPIService
         $this->ManagerAccountCustomerId =  str_replace("-", "", config('services.google.adwords.manager_account.customer_id'));
     }
 
+    public function getAccessibleCustomers(GoogleAccount $googleAccount, $repeatCall = false)
+    {
+        $url = "https://googleads.googleapis.com/v10/customers:listAccessibleCustomers";
+
+        $response = Http::withHeaders([
+            'developer-token' => $this->adwordsDeveloperToken,
+            'login-customer-id' => $this->ManagerAccountCustomerId
+        ])->withToken($googleAccount->token)->get($url);
+        Log::channel('google')->error("Adwords API Response: ", ['response' => $response]);
+
+        if ($response->status() == 400 && !$repeatCall) {
+            // This code block only checks if google accounts can be fetched after refreshing access token
+            if ($this->refreshToken($googleAccount) == false) {
+                return false;
+            } else {
+                $gAK = $this->getAccessibleCustomers($googleAccount, true);
+                // On success it returns google analytics accounts else false
+                if ($gAK !== false) {
+                    return $gAK;
+                } else {
+                    return false;
+                }
+            }
+        } else if ($response->status() == 401 && $repeatCall) {
+            return false;
+        }
+
+        $simpleXML = simplexml_load_string($response->body());
+
+        if (!$simpleXML->table->row->count()) {
+            return false;
+        }
+
+        $keywords = [];
+        foreach ($simpleXML->table->row as $row) {
+            $arr = [];
+            foreach ($row->attributes() as $key => $value) {
+                $arr[$key] = $value->__toString();
+            }
+            array_push($keywords, $arr);
+        }
+        return $keywords;
+    }
+
     public function getCampaignData(GoogleAccount $googleAccount, $repeatCall = false)
     {
         $url = "https://googleads.googleapis.com/v10/customers/" . str_replace("-", "", $googleAccount->adwords_client_customer_id) . "/googleAds:searchStream";
