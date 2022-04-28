952f0b8827a5b55517482f380cd4cb9e645ae0b4
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-04-28 10:08:51 +0500

    Formatted page view

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -3,7 +3,7 @@
 @section('page-title','Edit Users')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
