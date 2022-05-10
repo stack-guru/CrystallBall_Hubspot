125b165bef2aefce36bb552ddfe2701f2d3568a6
Ameer Hamza <hamzait2017@gmail.com>
2022-05-10 21:43:17 +0500

    add coming soon popup for Google Ads button on analytics page

--- resources/js/react/UserInterface/components/analyticsAndBusinessIntelligence/Index.js
+++ resources/js/react/UserInterface/components/analyticsAndBusinessIntelligence/Index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
 import VideoModalBox from '../../utils/VideoModalBox';
 
 
@@ -40,13 +40,13 @@ export default class AnalyticsAndBusinessIntelligenceIndex extends React.Compone
 
     render() {
         return (
-            <div className="container-xl bg-white  component-wrapper" >
+            <div className="container-xl bg-white  component-wrapper">
                 <section className="ftco-section" id="buttons">
                     <div className="container">
                         <div className="row mb-5">
                             <div className="col-md-12">
                                 <h2 className="heading-section gaa-title">
-                                    Analytics &amp; BI<br />
+                                    Analytics &amp; BI<br/>
                                     <small>Display your annotations on BI and Analytics tools</small>
                                 </h2>
                             </div>
@@ -54,36 +54,50 @@ export default class AnalyticsAndBusinessIntelligenceIndex extends React.Compone
 
                         <div className="row">
                             <div className="col-md-12">
-                                <ErrorAlert errors={this.state.errors} />
+                                <ErrorAlert errors={this.state.errors}/>
                             </div>
                         </div>
 
                         <div className="row">
                             <div className="col-lg-3 col-sm-4 text-center">
-                                <a target="_blank" href="https://chrome.google.com/webstore/detail/automated-google-analytic/jfkimpgkmamkdhamnhabohpeaplbpmom?hl=en">
-                                    <img style={{ width: '90%', height: 'auto' }} src="/images/buttons/google-analytics.png" />
+                                <a target="_blank"
+                                   href="https://chrome.google.com/webstore/detail/automated-google-analytic/jfkimpgkmamkdhamnhabohpeaplbpmom?hl=en">
+                                    <img style={{width: '90%', height: 'auto'}}
+                                         src="/images/buttons/google-analytics.png"/>
                                 </a>
-                                <br />
-                                <a target="_blank" href="https://www.youtube.com/watch?v=YLhIBKcFwFI">See how it works</a>
+                                <br/>
+                                <a target="_blank" href="https://www.youtube.com/watch?v=YLhIBKcFwFI">See how it
+                                    works</a>
                             </div>
                             <div className="col-lg-3 col-sm-4 text-center">
                                 <a target="_blank" href="https://datastudio.google.com/data?search=gaannotations">
-                                    <img style={{ width: '90%', height: 'auto' }} src="/images/buttons/google-data-studio.png" />
+                                    <img style={{width: '90%', height: 'auto'}}
+                                         src="/images/buttons/google-data-studio.png"/>
                                 </a>
-                                <br />
+                                <br/>
                                 <a target="_blank" href="https://youtu.be/rIOB3Pc3N5E">See how it works</a>
                             </div>
                             <div className="col-lg-3 col-sm-4 text-center">
-                                <a className="disabled" href="#" onClick={e => { e.preventDefault(); swal.fire('Coming Soon!', '', 'info'); }}>
-                                    <img style={{ width: '90%', height: 'auto' }} src="/images/buttons/microsoft-power-business-intelligence-cs.png" />
+                                <a className="disabled" href="#" onClick={e => {
+                                    e.preventDefault();
+                                    swal.fire('Coming Soon!', '', 'info');
+                                }}>
+                                    <img style={{width: '90%', height: 'auto'}}
+                                         src="/images/buttons/microsoft-power-business-intelligence-cs.png"/>
                                 </a>
-                                <br />
+                                <br/>
                             </div>
                             <div className="col-lg-3 col-sm-4 text-center">
-                                <a target="_blank" href="https://chrome.google.com/webstore/detail/automated-google-analytic/jfkimpgkmamkdhamnhabohpeaplbpmom?hl=en">
-                                    <img style={{ width: '90%', height: 'auto' }} src="/images/buttons/google-ads.svg" />
+                                {/*
+                                    href="https://chrome.google.com/webstore/detail/automated-google-analytic/jfkimpgkmamkdhamnhabohpeaplbpmom?hl=en"
+                                */}
+                                <a target="_blank" href="#" onClick={e => {
+                                    e.preventDefault();
+                                    swal.fire('Coming Soon!', '', 'info');
+                                }}>
+                                    <img style={{width: '90%', height: 'auto'}} src="/images/buttons/google-ads.svg"/>
                                 </a>
-                                <br />
+                                <br/>
                                 {/* <a target="_blank" href="https://youtu.be/rIOB3Pc3N5E">See how it works</a> */}
                             </div>
                         </div>
