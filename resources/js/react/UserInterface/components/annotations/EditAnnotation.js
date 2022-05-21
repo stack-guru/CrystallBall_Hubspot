4a03b80b6d1dfde38c0a022aecc8be213dbd197f
Hamees Ahmed Khan <hamees.sublimelogics@gmail.com>
2022-05-21 13:41:27 +0500

    Removed on focus code from edit annotation page

--- resources/js/react/UserInterface/components/annotations/EditAnnotation.js
+++ resources/js/react/UserInterface/components/annotations/EditAnnotation.js
@@ -268,32 +268,6 @@ export default class EditAnnotation extends React.Component {
                                             onChangeCallback2={this.gAPropertyChangeHandler}
                                             placeholder="Select GA Properties"
                                             multiple
-                                            onFocus={(e) => {
-                                                if (this.props.currentPricePlan.ga_account_count == 1 || this.props.currentPricePlan.google_analytics_property_count == -1) {
-                                                    const accountNotLinkedHtml = '' +
-                                                        '<div class="">' +
-                                                        '<img src="/images/property-upgrade-modal.jpg" class="img-fluid">' +
-                                                        '</div>'
-                                                    /*
-                                                    * Show new google analytics account popup
-                                                    * */
-                                                    swal.fire({
-                                                        html: accountNotLinkedHtml,
-                                                        width: 700,
-                                                        customClass: {
-                                                            popup: 'custom_bg pb-5',
-                                                            htmlContainer: 'm-0',
-                                                        },
-                                                        confirmButtonClass: "rounded-pill btn btn-primary bg-primary px-4 font-weight-bold",
-                                                        confirmButtonText: "Upgrade Now" + "<i class='ml-2 fa fa-caret-right'> </i>",
-
-                                                    }).then(value => {
-                                                        if (value.isConfirmed) {
-                                                            this.setState({ redirectTo: "/settings/price-plans" });
-                                                        }
-                                                    });
-                                                }
-                                            }}
                                         ></GoogleAnalyticsPropertySelect>
                                     </div>
                                 </div>
