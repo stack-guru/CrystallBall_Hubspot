86d5d40035083b9e94a26d1e48906853d27ba9ff
Ameer Hamza <hamzait2017@gmail.com>
2022-08-25 14:35:22 +0500

    add instagram graph api

--- app/Http/Controllers/InstagramAutomationController.php
+++ app/Http/Controllers/InstagramAutomationController.php
@@ -9,15 +9,16 @@ class InstagramAutomationController extends Controller
 {
     public function redirectInstagram(Request $request)
     {
-        return Socialite::driver('instagrambasic')->scopes([
-            'instagram_graph_user_media',
-            'instagram_graph_user_profile'
+        return Socialite::driver('instagram')->scopes([
+            'instagram_basic',
+            'pages_show_list',
+            'instagram_manage_insights',
         ])->redirect();
     }
 
     public function callbackInstagram(Request $request)
     {
-        $user = Socialite::driver('instagrambasic')->user();
+        $user = Socialite::driver('instagram')->user();
         dd($request->all(), $user);
     }
 }
