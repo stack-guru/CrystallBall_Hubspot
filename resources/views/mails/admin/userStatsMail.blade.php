d57b3dd10abf3c8194284824ddf59ee0d99f8bf0
Ameer Hamza <hamzait2017@gmail.com>
2022-09-07 14:39:38 +0500

    user stats email ui changes

--- resources/views/mails/admin/userStatsMail.blade.php
+++ resources/views/mails/admin/userStatsMail.blade.php
@@ -50,142 +50,151 @@
 
                             <div class="card-body">
                                 <h3 class="">Activity and Traction</h3>
-                                <table class="table table-borderd table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Active Users Yesterday</th>
-                                            <th>Active Users 30 Days</th>
-                                            <th>Active Users 60 Days</th>
-                                            <th>Active Users 90 Days</th>
-                                            <th>Total Registrations</th>
-                                            <th>Yesterday Registrations</th>
-                                            <th>Current Month Registrations</th>
-                                            <th>Previous Month Registrations</th>
-                                            <th>New Paying Users Yesterday</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                {{ @$data['active_users_yesterday'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['active_users_in_30_days'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['active_users_in_60_days'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['active_users_in_90_days'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['total_registration_count'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['yesterday_registration_count'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['current_month_registration_count'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['previous_month_registration_count'] }}
-                                            </td>
-                                            <td>
-                                                {{ @$data['new_paying_users_yesterday_count'] }}
-                                            </td>
-                                        </tr>
-                                    </tbody>
-
-                                </table>
-
-                                <div class="my-5">
-                                    <h3>Users Registered Yesterday</h3>
+                                <div style="overflow-x:auto;">
                                     <table class="table table-borderd table-striped">
                                         <thead>
                                             <tr>
-                                                <th>Name</th>
-                                                <th>Email</th>
+                                                <th>Active Users Yesterday</th>
+                                                <th>Active Users 30 Days</th>
+                                                <th>Active Users 60 Days</th>
+                                                <th>Active Users 90 Days</th>
+                                                <th>Total Registrations</th>
+                                                <th>Yesterday Registrations</th>
+                                                <th>Current Month Registrations</th>
+                                                <th>Previous Month Registrations</th>
+                                                <th>New Paying Users Yesterday</th>
                                             </tr>
                                         </thead>
-
+    
                                         <tbody>
-                                            @if (isset($data['yesterday_registration_users']))
-                                                @foreach ($data['yesterday_registration_users'] as $email=>$name)
+                                            <tr>
+                                                <td>
+                                                    {{ @$data['active_users_yesterday'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['active_users_in_30_days'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['active_users_in_60_days'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['active_users_in_90_days'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['total_registration_count'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['yesterday_registration_count'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['current_month_registration_count'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['previous_month_registration_count'] }}
+                                                </td>
+                                                <td>
+                                                    {{ @$data['new_paying_users_yesterday_count'] }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+    
+                                    </table>
+                                </div>
+                                
+
+                                <div class="my-5">
+                                    <h3>Users Registered Yesterday</h3>
+                                    <div style="overflow-x:auto;">
+                                        <table class="table table-borderd table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Email</th>
+                                                </tr>
+                                            </thead>
+    
+                                            <tbody>
+                                                @if (isset($data['yesterday_registration_users']))
+                                                    @foreach ($data['yesterday_registration_users'] as $email=>$name)
+                                                        <tr>
+                                                            <td>
+                                                                {{ $name }}
+                                                            </td>
+                                                            <td>
+                                                                {{ $email }}
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                @else
                                                     <tr>
-                                                        <td>
-                                                            {{ $name }}
-                                                        </td>
-                                                        <td>
-                                                            {{ $email }}
+                                                        <td colspan="2">
+                                                            No new registrations yesterday..
                                                         </td>
                                                     </tr>
-                                                @endforeach
-                                            @else
-                                                <tr>
-                                                    <td colspan="2">
-                                                        No new registrations yesterday..
-                                                    </td>
-                                                </tr>
-                                            @endif
-
-                                        </tbody>
-
-                                    </table>
+                                                @endif
+    
+                                            </tbody>
+    
+                                        </table>
+                                    </div>
+                                    
                                 </div>
 
 
                                 <div class="my-5">
                                     <h3>New Paying Users Yesterday</h3>
-                                    <table class="table table-borderd table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Email</th>
-                                                <th>Plan</th>
-                                                <th>Charged Price</th>
-                                            </tr>
-                                        </thead>
-
-                                        <tbody>
-                                            @if (isset($data['new_paying_users_yesterday']))
-                                                @foreach ($data['new_paying_users_yesterday'] as $price_plan_sub)
-                                                    <tr>
-                                                        <td>
-                                                            {{ $price_plan_sub->user->name ?? '' }}
-                                                        </td>
-                                                        <td>
-                                                            {{ $price_plan_sub->user->email ?? '' }}
-                                                        </td>
-                                                        <td>
-                                                            {{ $price_plan_sub->pricePlan->name ?? '' }}
-                                                            -
-                                                            @if ($price_plan_sub->user->lastPricePlanSubscription && $price_plan_sub->user->lastPricePlanSubscription->plan_duration)
-                                                                @if ($price_plan_sub->user->lastPricePlanSubscription->plan_duration == 12)
-                                                                    Annually
-                                                                @elseif($price_plan_sub->user->lastPricePlanSubscription->plan_duration == 1)
-                                                                    Monthly
+                                    <div style="overflow-x:auto;">
+                                        <table class="table table-borderd table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Email</th>
+                                                    <th>Plan</th>
+                                                    <th>Charged Price</th>
+                                                </tr>
+                                            </thead>
+    
+                                            <tbody>
+                                                @if (isset($data['new_paying_users_yesterday']))
+                                                    @foreach ($data['new_paying_users_yesterday'] as $price_plan_sub)
+                                                        <tr>
+                                                            <td>
+                                                                {{ $price_plan_sub->user->name ?? '' }}
+                                                            </td>
+                                                            <td>
+                                                                {{ $price_plan_sub->user->email ?? '' }}
+                                                            </td>
+                                                            <td>
+                                                                {{ $price_plan_sub->pricePlan->name ?? '' }}
+                                                                -
+                                                                @if ($price_plan_sub->user->lastPricePlanSubscription && $price_plan_sub->user->lastPricePlanSubscription->plan_duration)
+                                                                    @if ($price_plan_sub->user->lastPricePlanSubscription->plan_duration == 12)
+                                                                        Annually
+                                                                    @elseif($price_plan_sub->user->lastPricePlanSubscription->plan_duration == 1)
+                                                                        Monthly
+                                                                    @endif
+                                                                @else
+                                                                    N/A
                                                                 @endif
-                                                            @else
-                                                                N/A
-                                                            @endif
-                                                        </td>
-                                                        <td>
-                                                            {{ $price_plan_sub->paymentDetail->charged_price ?? '' }}
+                                                            </td>
+                                                            <td>
+                                                                {{ $price_plan_sub->paymentDetail->charged_price ?? '' }}
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                @else
+                                                    <tr>
+                                                        <td colspan="4">
+                                                            No new paying users yesterday..
                                                         </td>
                                                     </tr>
-                                                @endforeach
-                                            @else
-                                                <tr>
-                                                    <td colspan="4">
-                                                        No new paying users yesterday..
-                                                    </td>
-                                                </tr>
-                                            @endif
-
-                                        </tbody>
-
-                                    </table>
+                                                @endif
+    
+                                            </tbody>
+    
+                                        </table>
+                                    </div>
+                                    
                                 </div>
 
 
