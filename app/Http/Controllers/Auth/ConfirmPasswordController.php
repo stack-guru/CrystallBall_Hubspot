282d153a1f23f3adbc03d44cbe74e1d4fa01df81
codingwithhassan <codingwithme.hassan@gmail.com>
2022-10-12 17:57:45 +0500

    verify email address and then generate password

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\Rules\HasLettersNumbers;
+use App\Rules\HasSymbol;
 use Illuminate\Foundation\Auth\ConfirmsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ConfirmPasswordController extends Controller
 {
@@ -35,6 +40,21 @@ class ConfirmPasswordController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['generatePassword']);
+    }
+
+    public function generatePassword(Request $request)
+    {
+        $this->validate($request, [
+            'password' => ['confirmed', 'required', 'string', 'min:8', new HasSymbol, new HasLettersNumbers]
+        ], [
+            'password.min' => 'Must be at least 8 characters.',
+        ]);
+
+        $request->user()->forceFill([
+            'password' => Hash::make($request->password)
+        ])->save();
+
+        return redirect($this->redirectPath());
     }
 }
