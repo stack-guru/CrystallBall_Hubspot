09ffe986685940aba554243cbbb56070fd0500d6
Ameer Hamza <hamzait2017@gmail.com>
2022-10-14 17:54:41 +0500

    make users verified

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 //Route::get('revoke_tokens', function (){
 //    $users = \App\Models\User::all();
