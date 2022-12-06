c7dfce91cc0d0092689d9b55e93d1766f56cbd57
Hamza Aslam <imhamza@outlook.com>
2022-12-06 15:54:03 +0500

    Disabled throttle limit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            // 'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
