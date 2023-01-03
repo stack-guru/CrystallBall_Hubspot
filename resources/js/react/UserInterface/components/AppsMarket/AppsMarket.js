caf18cb578926477e092b90670f01205b444a0e7
maxifjaved <axif.javed@gmail.com>
2023-01-03 11:16:10 +0500

    github ui added to the modal

--- resources/js/react/UserInterface/components/AppsMarket/AppsMarket.js
+++ resources/js/react/UserInterface/components/AppsMarket/AppsMarket.js
@@ -38,6 +38,7 @@ import WeatherAlerts from "./WeatherAlerts";
 import WordpressUpdates from "./WordpressUpdates";
 import RankTracking from "./RankTracking";
 import Bitbucket from "./Bitbucket";
+import Github from "./Github";
 
 class AppsMarket extends React.Component {
     constructor(props) {
@@ -951,6 +952,31 @@ class AppsMarket extends React.Component {
                         ) : this.state.dsKey ===
                           "is_ds_bitbucket_tracking_enabled" ? (
                             <Bitbucket
+                                {...this.state}
+                                {...this.props}
+                                updateUserAnnotationColors={
+                                    this.updateUserAnnotationColors
+                                }
+                                serviceStatusHandler={this.serviceStatusHandler}
+                                changeShownHint={this.changeShownHint}
+                                sectionToggler={this.sectionToggler}
+                                userDataSourceAddHandler={
+                                    this.userDataSourceAddHandler
+                                }
+                                userDataSourceDeleteHandler={
+                                    this.userDataSourceDeleteHandler
+                                }
+                                reloadWebMonitors={this.reloadWebMonitors}
+                                loadUserDataSources={this.loadUserDataSources}
+                                updateGAPropertyId={(value) => {
+                                    this.setState({
+                                        ga_property_id: value,
+                                    });
+                                }}
+                            />
+                        ) : this.state.dsKey ===
+                          "is_ds_github_tracking_enabled" ? (
+                            <Github
                                 {...this.state}
                                 {...this.props}
                                 updateUserAnnotationColors={
