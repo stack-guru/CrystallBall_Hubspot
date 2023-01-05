2fabd2c3b1289e5861fc647430872d7afabc5c40
maxifjaved <axif.javed@gmail.com>
2023-01-05 11:53:40 +0500

    popups structure updated

--- resources/js/react/UserInterface/utils/TwitterTracking.js
+++ resources/js/react/UserInterface/utils/TwitterTracking.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HttpClient from "./HttpClient";
-import Toast from '../utils/Toast';
+import Toast from "../utils/Toast";
 
 export default class FacebookTracking extends React.Component {
     constructor(props) {
@@ -25,46 +25,68 @@ export default class FacebookTracking extends React.Component {
 
     fetchConfigurations() {
         /*
-        * this function will fetch configurations for given user
-        * */
-        this.setState({ isBusy: true })
-        HttpClient.get('/data-source/get-twitter-tracking-configurations').then(resp => {
-
-            this.setState({
-                is_tweets_likes_tracking_on: resp.data.is_tweets_likes_tracking_on,
-                when_tweet_reach_likes: resp.data.when_tweet_reach_likes,
-                is_tweets_retweets_tracking_on: resp.data.is_tweets_retweets_tracking_on,
-                when_tweet_reach_retweets: resp.data.when_tweet_reach_retweets,
+         * this function will fetch configurations for given user
+         * */
+        this.setState({ isBusy: true });
+        HttpClient.get("/data-source/get-twitter-tracking-configurations")
+            .then(
+                (resp) => {
+                    this.setState({
+                        is_tweets_likes_tracking_on:
+                            resp.data.is_tweets_likes_tracking_on,
+                        when_tweet_reach_likes:
+                            resp.data.when_tweet_reach_likes,
+                        is_tweets_retweets_tracking_on:
+                            resp.data.is_tweets_retweets_tracking_on,
+                        when_tweet_reach_retweets:
+                            resp.data.when_tweet_reach_retweets,
+                    });
+                    document.getElementById(
+                        "is_tweets_likes_tracking_on_checkbox"
+                    ).checked = resp.data.is_tweets_likes_tracking_on ?? 0;
+                    document.getElementById("when_tweet_reach_likes").value =
+                        resp.data.when_tweet_reach_likes ?? 0;
+                    document.getElementById(
+                        "is_tweets_retweets_tracking_on_checkbox"
+                    ).checked = resp.data.is_tweets_retweets_tracking_on ?? 0;
+                    document.getElementById("when_tweet_reach_retweets").value =
+                        resp.data.when_tweet_reach_retweets ?? 0;
+                },
+                (err) => {
+                    this.setState({ isBusy: false, errors: err.response.data });
+                }
+            )
+            .catch((err) => {
+                this.setState({ isBusy: false, errors: err });
             });
-            document.getElementById('is_tweets_likes_tracking_on_checkbox').checked = resp.data.is_tweets_likes_tracking_on ?? 0;
-            document.getElementById('when_tweet_reach_likes').value = resp.data.when_tweet_reach_likes ?? 0;
-            document.getElementById('is_tweets_retweets_tracking_on_checkbox').checked = resp.data.is_tweets_retweets_tracking_on ?? 0;
-            document.getElementById('when_tweet_reach_retweets').value = resp.data.when_tweet_reach_retweets ?? 0;
-
-        }, (err) => {
-            this.setState({ isBusy: false, errors: (err.response).data });
-        }).catch(err => {
-            this.setState({ isBusy: false, errors: err });
-        });
     }
 
     onSubmitHandler() {
         let form_data = {
             is_tweets_likes_tracking_on: this.state.is_tweets_likes_tracking_on,
             when_tweet_reach_likes: this.state.when_tweet_reach_likes,
-            is_tweets_retweets_tracking_on: this.state.is_tweets_retweets_tracking_on,
+            is_tweets_retweets_tracking_on:
+                this.state.is_tweets_retweets_tracking_on,
             when_tweet_reach_retweets: this.state.when_tweet_reach_retweets,
-        }
-        HttpClient.post('/data-source/save-twitter-tracking-configurations', form_data).then(resp => {
-            Toast.fire({
-                icon: 'success',
-                title: 'Stored successfully!'
-            })
-        }, (err) => {
-            this.setState({ isBusy: false, errors: (err.response).data });
-        }).catch(err => {
-            this.setState({ isBusy: false, errors: err });
-        });
+        };
+        HttpClient.post(
+            "/data-source/save-twitter-tracking-configurations",
+            form_data
+        )
+            .then(
+                (resp) => {
+                    Toast.fire({
+                        icon: "success",
+                        title: "Stored successfully!",
+                    });
+                },
+                (err) => {
+                    this.setState({ isBusy: false, errors: err.response.data });
+                }
+            )
+            .catch((err) => {
+                this.setState({ isBusy: false, errors: err });
+            });
     }
 
     render() {
@@ -75,66 +97,94 @@ export default class FacebookTracking extends React.Component {
                     <div className="w-100 mb-3">
                         <p>Trigger latest 100 tweets from account timeline</p>
                     </div>
+                    <div className="white-box">
+                        <h5 className="gaa-text-primary">
+                            <b>Create Annotation When:</b>
+                        </h5>
 
-                    <h5 className="gaa-text-primary"><b>Create Annotation When:</b></h5>
-
-                    <div className="mt-2">
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox" checked={this.state.is_tweets_likes_tracking_on}
-                                id="is_tweets_likes_tracking_on_checkbox" onChange={(e) => {
-                                    this.setState({
-                                        is_tweets_likes_tracking_on: e.target.checked
-                                    })
-                                }} />
-                            <label className="form-check-label" htmlFor="is_tweets_likes_tracking_on_checkbox">
-                                A Post that raised
+                        <div className="mt-2">
+                            <div className="form-check">
                                 <input
-                                    id="when_tweet_reach_likes"
-                                    onKeyUp={(e) => {
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    checked={
+                                        this.state.is_tweets_likes_tracking_on
+                                    }
+                                    id="is_tweets_likes_tracking_on_checkbox"
+                                    onChange={(e) => {
                                         this.setState({
-                                            when_tweet_reach_likes: e.target.value
-                                        })
+                                            is_tweets_likes_tracking_on:
+                                                e.target.checked,
+                                        });
                                     }}
-                                    className="d-inline border m-1 mx-3 text-center p-1 w-25" />
-                                Likes
-                            </label>
+                                />
+                                <label
+                                    className="form-check-label"
+                                    htmlFor="is_tweets_likes_tracking_on_checkbox"
+                                >
+                                    A Post that raised
+                                    <input
+                                        id="when_tweet_reach_likes"
+                                        onKeyUp={(e) => {
+                                            this.setState({
+                                                when_tweet_reach_likes:
+                                                    e.target.value,
+                                            });
+                                        }}
+                                        className="d-inline border m-1 mx-3 text-center p-1 w-25"
+                                    />
+                                    Likes
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="mt-2">
+                            <div className="form-check">
+                                <input
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    checked={
+                                        this.state
+                                            .is_tweets_retweets_tracking_on
+                                    }
+                                    id="is_tweets_retweets_tracking_on_checkbox"
+                                    onChange={(e) => {
+                                        this.setState({
+                                            is_tweets_retweets_tracking_on:
+                                                e.target.checked,
+                                        });
+                                    }}
+                                />
+                                <label
+                                    className="form-check-label"
+                                    htmlFor="is_tweets_retweets_tracking_on_checkbox"
+                                >
+                                    A Post that raised
+                                    <input
+                                        id="when_tweet_reach_retweets"
+                                        onKeyUp={(e) => {
+                                            this.setState({
+                                                when_tweet_reach_retweets:
+                                                    e.target.value,
+                                            });
+                                        }}
+                                        className="d-inline border m-1 mx-3 text-center p-1 w-25"
+                                    />
+                                    Retweets
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="mt-4">
+                            <button
+                                className="btn btn-success"
+                                onClick={this.onSubmitHandler}
+                            >
+                                Save
+                            </button>
                         </div>
                     </div>
-
-                    <div className="mt-2">
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox" checked={this.state.is_tweets_retweets_tracking_on}
-                                id="is_tweets_retweets_tracking_on_checkbox" onChange={(e) => {
-                                    this.setState({
-                                        is_tweets_retweets_tracking_on: e.target.checked
-                                    })
-                                }} />
-                            <label className="form-check-label" htmlFor="is_tweets_retweets_tracking_on_checkbox">
-                                A Post that raised
-                                <input
-                                    id="when_tweet_reach_retweets"
-                                    onKeyUp={(e) => {
-                                        this.setState({
-                                            when_tweet_reach_retweets: e.target.value
-                                        })
-                                    }}
-                                    className="d-inline border m-1 mx-3 text-center p-1 w-25" />
-                                Retweets
-                            </label>
-                        </div>
-                    </div>
-
-                    <div className="mt-4">
-                        <button
-                            className="btn btn-success"
-                            onClick={this.onSubmitHandler}
-                        >
-                            Save
-                        </button>
-                    </div>
-
                 </div>
-
             </div>
         );
     }
