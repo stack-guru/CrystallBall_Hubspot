c671b023cccb39c63b8ade485f67a7d40b06880f
Xuecheng Jin <enjoydhoney@gmail.com>
2023-01-14 01:23:53 +0900

    Signup with company mail

---
+++ app/Models/websiteTechnologyLookup.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class websiteTechnologyLookup extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'site_url',
+        'tech1',
+        'tech2',
+        'tech3',
+        'tech4',
+        'tech5',
+        'tech6',
+        'tech7',
+        'tech8',
+        'tech9',
+        'tech10',
+        'tech11',
+        'tech12',
+        'tech13',
+        'tech14',
+        'tech15',
+        'tech16',
+        'tech17',
+        'tech18',
+        'tech19',
+        'tech20',
+    ];
+}
