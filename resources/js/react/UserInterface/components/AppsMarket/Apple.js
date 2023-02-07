ddb873f9e8e30ecc90f44e0f60b14424c384859f
maxifjaved <axif.javed@gmail.com>
2023-02-08 02:51:06 +0500

    apple podcast creadits added

--- resources/js/react/UserInterface/components/AppsMarket/Apple.js
+++ resources/js/react/UserInterface/components/AppsMarket/Apple.js
@@ -3,19 +3,49 @@ import UserAnnotationColorPicker from "../../helpers/UserAnnotationColorPickerCo
 import { ApplePodcastConfig } from "../../utils/ApplePodcast";
 import ModalHeader from "./common/ModalHeader";
 import DescrptionModalNormal from "./common/DescriptionModalNormal";
+import HttpClient from "../../utils/HttpClient";
+import Toast from "../../utils/Toast";
 
 class Apple extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isRead: false
+            isRead: false,
+            applePodcast: []
         }
+        this.getExistingPodcasts = this.getExistingPodcasts.bind(this)
     }
 
     changeModal() {
         this.setState({isRead: true})
     }
 
+    getExistingPodcasts = async () => {
+        HttpClient.get(
+            `/data-source/apple-podcast-monitor?ga_property_id=${this.props.ga_property_id}`
+        )
+            .then(
+                (result) => {
+                    this.setState({applePodcast: result.data.apple_podcast_monitors })
+                },
+                (err) => {
+                    Toast.fire({
+                        icon: 'error',
+                        title: "Error while getting exists Apple Podcast.",
+                    });
+                }
+            )
+            .catch((err) => {
+                Toast.fire({
+                    icon: 'error',
+                    title: "Error while getting exists Apple Podcast.",
+                });
+            });
+    };
+
+    componentDidMount() {
+        this.getExistingPodcasts();
+    }
     render() {
         return (
             <div className='popupContent modal-apple'>
@@ -39,10 +69,10 @@ class Apple extends React.Component {
                     serviceName={"Apple Podcast"}
                     colorKeyName={"apple_podcast"}
                     dsKeyName={"is_ds_apple_podcast_annotation_enabled"}
-                    creditString={`TODO`}
+                    creditString={`${ this.state.applePodcast?.length } / ${ (this.props.user.price_plan.apple_podcast_monitor_count * 1) == -1 ? 0 : this.props.user.price_plan.apple_podcast_monitor_count}`}
                 />
 
-                <ApplePodcastConfig gaPropertyId={this.props.ga_property_id}/>
+                <ApplePodcastConfig limitReached={this.state.applePodcast?.length >= (this.props.user.price_plan.apple_podcast_monitor_count * 1)} existingPodcast={this.state.applePodcast} getExistingPodcasts={this.getExistingPodcasts} gaPropertyId={this.props.ga_property_id}/>
                 </>
                 }
             </div>
--- resources/js/react/UserInterface/utils/ApplePodcast/ApplePodcastConfig.js
+++ resources/js/react/UserInterface/utils/ApplePodcast/ApplePodcastConfig.js
@@ -20,40 +20,16 @@ import {
 import Slider from "react-slick";
 
 const ApplePodcastConfig = (props) => {
-    const [existingPodcast, setExisting] = useState([]);
     const [searchResult, setSearchResult] = useState([]);
     const [noResult, setNoResult] = useState("");
     const [inputVale, setInputVale] = useState("");
     const [activeDeletePopover, setActiveDeletePopover] = useState("");
 
-    const getExistingPodcasts = async () => {
-        HttpClient.get(
-            `/data-source/apple-podcast-monitor?ga_property_id=${props.gaPropertyId}`
-        )
-            .then(
-                (result) => {
-                    setExisting(result.data.apple_podcast_monitors);
-                },
-                (err) => {
-                    Toast.fire({
-                        icon: 'error',
-                        title: "Error while getting exists Apple Podcast.",
-                    });
-                }
-            )
-            .catch((err) => {
-                Toast.fire({
-                    icon: 'error',
-                    title: "Error while getting exists Apple Podcast.",
-                });
-            });
-    };
-
     const deletePodcasts = async (payload) => {
         HttpClient.delete(`/data-source/apple-podcast-monitor/${payload.id}`)
             .then(
                 () => {
-                    getExistingPodcasts();
+                    props.getExistingPodcasts();
                     Toast.fire({
                         icon: 'success',
                         title: "Apple Podcast deleted successfully.",
@@ -75,7 +51,7 @@ const ApplePodcastConfig = (props) => {
     };
 
     useEffect(() => {
-        getExistingPodcasts();
+        props.getExistingPodcasts();
     }, [props.gaPropertyId]);
 
     const getMetaData = async (ev) => {
@@ -112,31 +88,58 @@ const ApplePodcastConfig = (props) => {
     };
 
     const addAnnotation = async (formData) => {
-        Toast.fire({
-            icon: 'info',
-            title: "Creating Annotations",
-        });
-        HttpClient.post("/data-source/apple_podcast_url", formData)
-            .then(
-                () => {
-                    Toast.fire({
-                        icon: 'success',
-                        title: "Apple Podcast added successfully.",
-                    });
+        if (props.limitReached) {
+            const accountNotLinkedHtml =
+                "" +
+                '<div class="">' +
+                '<img src="/images/annotation_limit_reached.png" class="img-fluid">' +
+                "</div>";
+
+            swal.fire({
+                html: accountNotLinkedHtml,
+                width: 1000,
+                showCancelButton: true,
+                showCloseButton: true,
+                customClass: {
+                    popup: "themePlanAlertPopup",
+                    htmlContainer: "themePlanAlertPopupContent",
+                    closeButton: 'btn-closeplanAlertPopup',
                 },
-                (err) => {
+                cancelButtonClass: "btn-bookADemo",
+                cancelButtonText: "Book a Demo",
+                confirmButtonClass: "btn-subscribeNow",
+                confirmButtonText: "Subscribe now",
+            }).then((value) => {
+                if (value.isConfirmed) window.location.href = '/settings/price-plans'
+            });
+        } else {
+            Toast.fire({
+                icon: 'info',
+                title: "Creating Annotations",
+            });
+            HttpClient.post("/data-source/apple_podcast_url", formData)
+                .then(
+                    () => {
+                        Toast.fire({
+                            icon: 'success',
+                            title: "Apple Podcast added successfully.",
+                        });
+                    },
+                    (err) => {
+                        Toast.fire({
+                            icon: 'error',
+                            title: "Error while adding Apple Podcast.",
+                        });
+                    }
+                )
+                .catch((err) => {
                     Toast.fire({
                         icon: 'error',
                         title: "Error while adding Apple Podcast.",
                     });
-                }
-            )
-            .catch((err) => {
-                Toast.fire({
-                    icon: 'error',
-                    title: "Error while adding Apple Podcast.",
                 });
-            });
+        }
+
     };
 
     function SampleNextArrow(props) {
@@ -173,7 +176,7 @@ const ApplePodcastConfig = (props) => {
 
 
                 <div className="input-group mb-3">
-                    <input type="text" className="form-control search-input" placeholder="Search or Enter the Podcast URL" value={inputVale} id="applePodcastURL" name="applePodcastURL" onChange={(e) => setInputVale(e.target.value.toLowerCase())} onKeyUp={(e) => {if (e.keyCode === 13) { e.persist(); getMetaData(e);}}}/>
+                    <input type="text" className="form-control search-input" placeholder="Search or Enter the Podcast URL" value={inputVale} id="applePodcastURL" name="applePodcastURL" onChange={(e) => setInputVale(e.target.value.toLowerCase())} onKeyUp={(e) => { if (e.keyCode === 13) { e.persist(); getMetaData(e); } }} />
                     <div className="input-group-append">
                         <i className="ti-plus"></i>
                     </div>
@@ -185,7 +188,7 @@ const ApplePodcastConfig = (props) => {
                     <Slider {...settings}>
                         {searchResult.map((t0a) => (
                             <Card className="cb-ap-search-card apple-card mb-0" body>
-                                <CardImg top width="100%" src={t0a.previewImage} alt={t0a.collectionName}/>
+                                <CardImg top width="100%" src={t0a.previewImage} alt={t0a.collectionName} />
                                 <CardTitle tag="h5">{t0a.collectionName}</CardTitle>
                                 <CardSubtitle tag="h6" className="mb-0 text-muted">{t0a.trackCount} episodes</CardSubtitle>
                                 <Button onClick={() => addAnnotation(t0a)}>Create Annotations</Button>
@@ -208,14 +211,14 @@ const ApplePodcastConfig = (props) => {
                 ) : null} */}
             </div>
 
-            {existingPodcast.length ? <div className="white-box">
+            {props.existingPodcast.length ? <div className="white-box">
                 <h4 className='textblue'>Manage podcasts</h4>
                 <div className="gray-box">
                     <h4>
-                    Active podcasts: <span>(Click to remove)</span>
+                        Active podcasts: <span>(Click to remove)</span>
                     </h4>
                     <div className="d-flex keywordTags">
-                        {existingPodcast?.map((gAK) => {
+                        {props.existingPodcast?.map((gAK) => {
                             return (
                                 <>
                                     <button
