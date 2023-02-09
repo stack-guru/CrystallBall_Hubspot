dd18b69278735e13637f054cf0ba9aad42dbf05b
maxifjaved <axif.javed@gmail.com>
2023-02-09 17:19:45 +0500

    typo  change

--- resources/js/react/UserInterface/components/annotations/UploadAnnotation.js
+++ resources/js/react/UserInterface/components/annotations/UploadAnnotation.js
@@ -191,7 +191,7 @@ export default class UploadAnnotation extends React.Component {
                             </div>
                             <div className="themeNewInputStyle">
                                 <select name="date_format" id="date_format" className="form-control " value={this.state.date_format} onChange={this.changeHandler} required>
-                                    <option value="">select your date format</option>
+                                    <option value="">Select your date format</option>
                                     <option value="j/n/Y">{moment("2021-01-15").format('DD/MM/YYYY')}</option>
                                     <option value="n-j-Y">{moment("2021-01-15").format('M-D-YYYY')}</option>
                                     <option value="n-j-y">{moment("2021-01-15").format('M-D-YY')}</option>
