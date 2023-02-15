9c79cb645029aca5eee9e5e48f8ee6bd38ce726f
maxifjaved <axif.javed@gmail.com>
2023-02-15 16:37:10 +0500

    error popup fixed

---
+++ public/js/error-popup.js
@@ -0,0 +1,14 @@
+class TagComponent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<figure class="m-0"><img src="${this.getAttribute(
+            "bannerImg"
+        )}" alt="error image" /></figure>
+    <div class="d-flex flex-column gaErrorPopupContent">
+        ${this.getAttribute("heading")}
+        ${this.getAttribute("subHeading")}
+    </div>
+  `;
+    }
+}
+
+customElements.define(`ga-error-popup`, TagComponent);
