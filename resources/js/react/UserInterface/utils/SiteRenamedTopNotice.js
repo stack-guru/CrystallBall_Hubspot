f7c8a9961e1ae6525f38fa2cbc2efb2ecf3e07b8
maxifjaved <axif.javed@gmail.com>
2023-02-16 16:41:02 +0500

    updated the topbanner

--- resources/js/react/UserInterface/utils/SiteRenamedTopNotice.js
+++ resources/js/react/UserInterface/utils/SiteRenamedTopNotice.js
@@ -7,16 +7,16 @@ import TopNoticeBar from './TopNoticeBar';
 export default function SiteRenamedTopNotice(props) {
 
     let gannotations = () => IsDomain('app.gaannotations.com') || IsDomain('localhost') ? <p className="text-white" style={{ marginBottom: '0px' }}>
-            GAannotations is now <a style={{ color: 'white', textDecoration: 'underline' }} href="https://app.crystalballinsight.com">Crystal Ball</a>.
+            🥳 We have a New Look 🥳  {'>'} GAannotations is now <a style={{ color: 'white', textDecoration: 'underline' }} href="https://app.crystalballinsight.com">Crystal Ball</a>.
         </p> : null;
 
     return (
-        <TopNoticeBar show={props.show} backgroundColor="#096DB7"
+        <TopNoticeBar show={props.show} backgroundColor="linear-gradient(90deg, #FF8534 33.33%, #FF630C 100%)"
             content={
                 <div style={{ display: "flex", justifyContent: "center" }}>
                     { gannotations() } {" "}
                     <p className="text-white" style={{ marginBottom: '0px', marginLeft: '2px', marginRight: '2px', }}>
-                    Need Help? <a style={{ color: 'white', textDecoration: 'underline' }} href="https://calendly.com/crystal-ball/30min?back=&month=2023-01" target="_blank">Book a Demo</a>
+                    Need Help? <a style={{ color: 'white', textDecoration: 'underline' }} href="https://calendly.com/crystal-ball/30min" target="_blank">Book a Demo</a>
                     </p>
                 </div>
             }
