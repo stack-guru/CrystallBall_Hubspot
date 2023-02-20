e9696634ccde266bbe7213979e7b4f032b8f2599
Hamza Aslam <imhamza@outlook.com>
2023-02-20 13:26:48 +0500

    price plan line condition

--- resources/js/react/UserInterface/components/settings/pricingPlans/IndexPricingPlans.js
+++ resources/js/react/UserInterface/components/settings/pricingPlans/IndexPricingPlans.js
@@ -125,9 +125,9 @@ export default class IndexPricingPlans extends React.Component {
                     <Container>
                         <div className="pageHeader planPageHead">
                             <h2 className="pageTitle">Manage plan</h2>
-                            {/* {this.props.user.price_plan.name != "Free" && this.props.user.price_plan_expiry_date ?
+                            {this.props.user.price_plan.name == "Free" ?
                                 <p className='mb-0'>Your account will be automatically downgraded to the Free plan at {moment(this.props.user.price_plan_expiry_date || new Date()).format('ll')}. Upgrade your account to keep enjoying all the features.</p>
-                                : null} */}
+                                : null}
                         </div>
 
                         {userRegistrationOffer ? <div className="row ml-0 mr-0 p-2"><div className="col-12 text-center"><h2 className="gaa-title">{manipulateRegistrationOfferText(userRegistrationOffer.description, userRegistrationOffer)}</h2></div></div> : null}
