72377e50859b6d1d564fdbcf23bdb95c7e4c67be
Shahroz Rajpoot <m.shahroz6201@gmail.com>
2023-03-02 10:50:39 +0500

    Activated new user properties if Admin has active's

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -94,6 +94,83 @@ class UserController extends Controller
         $user->price_plan_id = $parentUser->price_plan_id;
         $user->price_plan_expiry_date = $parentUser->price_plan_expiry_date;
         $user->email_verified_at = now();
+
+        if ($parentUser->is_ds_holidays_enabled) {
+            $user->is_ds_holidays_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_google_algorithm_updates_enabled) {
+            $user->is_ds_google_algorithm_updates_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_retail_marketing_enabled) {
+            $user->is_ds_retail_marketing_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_weather_alerts_enabled) {
+            $user->is_ds_weather_alerts_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_google_alerts_enabled) {
+            $user->is_ds_google_alerts_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_wordpress_updates_enabled) {
+            $user->is_ds_wordpress_updates_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_web_monitors_enabled) {
+            $user->is_ds_web_monitors_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_apple_podcast_annotation_enabled) {
+            $user->is_ds_apple_podcast_annotation_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_shopify_annotation_enabled) {
+            $user->is_ds_shopify_annotation_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_g_ads_history_change_enabled) {
+            $user->is_ds_g_ads_history_change_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_anomolies_detection_enabled) {
+            $user->is_ds_anomolies_detection_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_budget_tracking_enabled) {
+            $user->is_ds_budget_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_keyword_tracking_enabled) {
+            $user->is_ds_keyword_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_bitbucket_tracking_enabled) {
+            $user->is_ds_bitbucket_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_github_tracking_enabled) {
+            $user->is_ds_github_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_facebook_tracking_enabled) {
+            $user->is_ds_facebook_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_instagram_tracking_enabled) {
+            $user->is_ds_instagram_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_twitter_tracking_enabled) {
+            $user->is_ds_twitter_tracking_enabled = 1;
+        }
+
+        if ($parentUser->is_ds_wordpress_enabled) {
+            $user->is_ds_wordpress_enabled = 1;
+        }
+
         $user->save();
 
         Mail::to($user)->send(new UserInviteMail($user, $request->password));
