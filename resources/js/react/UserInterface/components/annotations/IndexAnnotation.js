7bd1d24ce4d1933004ba99ff05d757a7229b7db9
maxifjaved <axif.javed@gmail.com>
2023-03-03 15:25:35 +0500

    delete issue fixes + stripHtmlTags from description

--- resources/js/react/UserInterface/components/annotations/IndexAnnotation.js
+++ resources/js/react/UserInterface/components/annotations/IndexAnnotation.js
@@ -16,6 +16,19 @@ import InfiniteScroll from 'react-infinite-scroller';
 import { capitalize, uniqBy } from "lodash";
 
 
+function stripHtmlTags(str) {
+    if (!str) {
+        return '';
+    }
+
+    if (typeof str !== 'string') {
+        // throw new TypeError('stripHtmlTags() expects a string argument.');
+        return '';
+    }
+
+    return str.replace(/<[^>]*>/g, '');
+}
+
 class IndexAnnotations extends React.Component {
 
     axiosCancelToken = null;
@@ -243,7 +256,7 @@ class IndexAnnotations extends React.Component {
                 (response) => {
                     this.loadAnnotationsCancelToken = null;
                     this.setState({
-                        annotations: uniqBy(this.state.annotations.concat(response.data.annotations), 'created_at'),
+                        annotations: uniqBy(this.state.annotations.concat(response.data.annotations), 'added_by'),
                         isLoading: false,
                         hasMore: response.data.annotations.length >= pageSize,
                     });
@@ -649,18 +662,18 @@ class IndexAnnotations extends React.Component {
                                                         )}
                                                     </p>
                                                     <p className="annotationDesc mb-0 d-flex-inline">
-                                                        {anno.description &&
-                                                            !anno.show_complete_desc ? anno.description.substring(0, 150) : ""}
-                                                        {anno.description &&
-                                                            anno.description.length > 150 &&
+                                                        {stripHtmlTags(anno.description) &&
+                                                            !anno.show_complete_desc ? stripHtmlTags(anno.description).substring(0, 150) : ""}
+                                                        {stripHtmlTags(anno.description) &&
+                                                        stripHtmlTags(anno.description).length > 150 &&
                                                             !anno.show_complete_desc ? (
                                                             <span>...<a onClick={() => { this.seeCompleteDescription(anno, idx); }} target="_blank" className="ml-1">Read more</a></span>
                                                         ) : (
                                                             ""
                                                         )}
 
-                                                        {anno.description && anno.description.length > 150 && anno.show_complete_desc ? (
-                                                            <div id="">{anno.description}</div>
+                                                        {stripHtmlTags(anno.description) && stripHtmlTags(anno.description).length > 150 && anno.show_complete_desc ? (
+                                                            <div id="">{stripHtmlTags(anno.description)}</div>
                                                         ) : (
                                                             ""
                                                         )}
