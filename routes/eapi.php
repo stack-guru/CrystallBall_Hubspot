dc82f62670d2fee5f38326c32a1f2b42c618ebff
Hamza Aslam <imhamza@outlook.com>
2023-03-06 11:50:50 +0500

    Revert Cookie Commit Changes

--- routes/eapi.php
+++ routes/eapi.php
@@ -21,18 +21,19 @@ Route::group(['namespace' => 'App\Http\Controllers', 'as' => 'eapi.', 'middlewar
 
     Route::group(['prefix' => 'v1/chrome-extension', 'as' => 'v1.chrome-extension.'], function () {
 
-        Route::get('csrf-cookie', function (Request $request) {
-            if ($request->expectsJson()) {
-                return new JsonResponse(['token' => csrf_token()], 200);
-            }
-            return new Response(csrf_token(), 200);
-        });
+        // Route::get('csrf-cookie', function (Request $request) {
+        //     if ($request->expectsJson()) {
+        //         return new JsonResponse(['token' => csrf_token()], 200);
+        //     }
+        //     return new Response(csrf_token(), 200);
+        // });
 
-        Route::post('login', 'API\ChromeExtension\LoginController@login')->middleware(['prevent.cache', 'guest'])->name('login');
-        Route::post('login/google', 'API\ChromeExtension\LoginController@loginWithGoogle')->middleware(['prevent.cache', 'guest'])->name('login.google');
-        Route::post('logout', 'API\ChromeExtension\LoginController@logout')->middleware(['prevent.cache'])->name('logout');
+        // Route::post('login', 'API\ChromeExtension\LoginController@login')->middleware(['prevent.cache', 'guest'])->name('login');
+        // Route::post('login/google', 'API\ChromeExtension\LoginController@loginWithGoogle')->middleware(['prevent.cache', 'guest'])->name('login.google');
+        // Route::post('logout', 'API\ChromeExtension\LoginController@logout')->middleware(['prevent.cache'])->name('logout');
 
-        Route::group(['middleware' => ['auth', 'verified']], function () {
+        // Route::group(['middleware' => ['auth', 'verified']], function () {
+        Route::group(['middleware' => ['auth:api', 'verified']], function () {
 
             Route::get('user', function (Request $request) {
                 return $request->user();
