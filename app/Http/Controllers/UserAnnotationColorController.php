5b9d090d2541378e4c5958c8f1ab115ca28faf83
Shahroz Rajpoot <m.shahroz6201@gmail.com>
2023-03-10 12:53:24 +0500

    Updated app default colors

--- app/Http/Controllers/UserAnnotationColorController.php
+++ app/Http/Controllers/UserAnnotationColorController.php
@@ -42,22 +42,22 @@ class UserAnnotationColorController extends Controller
 
         $defaultColor = '#1976fe';
         !str_contains($userAnnotationColor->manual, '#') ? $userAnnotationColor->manual = "#FE4C3C" : '';
-        !str_contains($userAnnotationColor->csv, '#') ? $userAnnotationColor->csv = $defaultColor : '';
+        !str_contains($userAnnotationColor->csv, '#') ? $userAnnotationColor->csv = "#17DE6B" : '';
         !str_contains($userAnnotationColor->api, '#') ? $userAnnotationColor->api = $defaultColor : '';
         !str_contains($userAnnotationColor->holidays, '#') ? $userAnnotationColor->holidays = $defaultColor : '';
         !str_contains($userAnnotationColor->google_algorithm_updates, '#') ? $userAnnotationColor->google_algorithm_updates = "#FFC514" : '';
         !str_contains($userAnnotationColor->retail_marketings, '#') ? $userAnnotationColor->retail_marketings = $defaultColor : '';
         !str_contains($userAnnotationColor->weather_alerts, '#') ? $userAnnotationColor->weather_alerts = "#FFC514" : '';
         !str_contains($userAnnotationColor->web_monitors, '#') ? $userAnnotationColor->web_monitors = $defaultColor : '';
-        !str_contains($userAnnotationColor->wordpress_updates, '#') ? $userAnnotationColor->wordpress_updates = "#1976fe" : '';
-        !str_contains($userAnnotationColor->google_alerts, '#') ? $userAnnotationColor->google_alerts = "#FFC514" : '';
+        !str_contains($userAnnotationColor->wordpress_updates, '#') ? $userAnnotationColor->wordpress_updates = "#253858" : '';
+        !str_contains($userAnnotationColor->google_alerts, '#') ? $userAnnotationColor->google_alerts = $defaultColor : '';
         !str_contains($userAnnotationColor->keyword_tracking, '#') ? $userAnnotationColor->keyword_tracking = $defaultColor : '';
         !str_contains($userAnnotationColor->facebook_tracking, '#') ? $userAnnotationColor->facebook_tracking = "#004f9d" : '';
         !str_contains($userAnnotationColor->bitbucket_tracking, '#') ? $userAnnotationColor->bitbucket_tracking = '#253858' : '';
         !str_contains($userAnnotationColor->github_tracking, '#') ? $userAnnotationColor->github_tracking = "#24292f" : '';
         !str_contains($userAnnotationColor->wordpress, '#') ? $userAnnotationColor->wordpress = "#1976fe" : '';
         !str_contains($userAnnotationColor->shopify, '#') ? $userAnnotationColor->shopify = "#00BB4F" : '';
-        !str_contains($userAnnotationColor->apple_podcast, '#') ? $userAnnotationColor->apple_podcast = "#ff6600" : '';
+        !str_contains($userAnnotationColor->apple_podcast, '#') ? $userAnnotationColor->apple_podcast = "#A00CE6" : '';
         $userAnnotationColor->save();
 
         return $userAnnotationColor;
