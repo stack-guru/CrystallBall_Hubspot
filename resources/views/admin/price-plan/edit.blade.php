ae9784b782aaefccbb991a16d7b24ea74b487a7d
Hamza Aslam <imhamza@outlook.com>
2023-03-14 20:41:40 +0500

    change text from price plan

--- resources/views/admin/price-plan/edit.blade.php
+++ resources/views/admin/price-plan/edit.blade.php
@@ -157,12 +157,12 @@
                         </div>
 
                         <div class="form-group">
-                            <label>Available? <sub>(if this plan can be purchased by a user from the price plan page )</sub></label>
+                            <label>Available? <sub>( This means? Show on pricing page? )</sub></label>
                             <input type="checkbox" name="is_available" id="isAvailable" class="form-control" @if($pricePlan->is_available) checked @endif />
                         </div>
 
                         <div class="form-group">
-                            <label>Enabled? <sub>(if the users can stay and show on this price plan page)</sub></label>
+                            <label>Enabled? <sub>( What is this means? stay where? )</sub></label>
                             <input type="checkbox" name="is_enabled" id="isEnabled" class="form-control" @if($pricePlan->is_enabled) checked @endif />
                         </div>
 
