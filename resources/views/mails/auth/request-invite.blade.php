83ae94cd53e55ec027b95efaf28c8d1008505e99
Shahroz Rajpoot <m.shahroz6201@gmail.com>
2023-03-14 14:10:37 +0500

    Implemented invitation request logic

---
+++ resources/views/mails/auth/request-invite.blade.php
@@ -0,0 +1,17 @@
+@component('mail::message')
+
+<p>Contact Email: {{ $user->email }}!</p>
+<br/>
+<p>Hi {{ $admin->name }},</p>
+<p>
+    {{ $user->email }} wants to use Crystal Ball as a part of your team. You can add team members <a href="{{ $addMemberLink }}">HERE</a>
+</p>
+
+<br/>
+<br/>
+<br/>
+
+Contact us for support.<br />
+The Crystal Ball Team.<br />
+<hr />
+@endcomponent
