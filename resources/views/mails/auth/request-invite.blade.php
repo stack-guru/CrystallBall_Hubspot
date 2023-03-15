be0ffbcd5ebedeac17014b8698af17b7733eb592
maxifjaved <axif.javed@gmail.com>
2023-03-15 12:16:40 +0500

    email text changes

--- resources/views/mails/auth/request-invite.blade.php
+++ resources/views/mails/auth/request-invite.blade.php
@@ -1,17 +1,15 @@
 @component('mail::message')
 
-<p>Contact Email:</p>
-<br/>
 <p>Hi {{ $admin->name }},</p>
 <p>
-    {{ $user->email }} wants to use Crystal Ball as a part of your team. You can add team members <a href="{{ $addMemberLink }}">HERE</a>
+    {{ $user->email }} wants to use Crystal Ball (GAannotations) as a part of your team. You can add team members <a href="{{ $addMemberLink }}">HERE</a>
 </p>
 
 <br/>
-<br/>
-<br/>
 
-Contact us for support.<br />
+Need help? Contact us for support.
+<br />
+<br />
 The Crystal Ball Team.<br />
 <hr />
 @endcomponent
