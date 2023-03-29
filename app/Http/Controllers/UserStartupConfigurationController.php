eab7d29527f1016100c9a980bc40b2796d1445b5
maxifjaved <axif.javed@gmail.com>
2023-03-29 15:44:05 +0500

    fixed carbon not found

--- app/Http/Controllers/UserStartupConfigurationController.php
+++ app/Http/Controllers/UserStartupConfigurationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserStartupConfigurationRequest;
 use App\Models\UserStartupConfiguration;
