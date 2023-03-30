d2f6b735a321f323195e862d69ee8c31e869a9c5
Hamza Aslam <imhamza@outlook.com>
2023-03-30 15:26:16 +0500

    revert changes for extension

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -21,7 +21,7 @@ class CORS
             ->header('Access-Control-Allow-Origin', '*')
             // ->header('Access-Control-Allow-Origin', 'https://ads.google.com, https://analytics.google.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
-            // ->header('Access-Control-Allow-Credentials', 'true');
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
 
-    // 'allowed_origins' => [],
+    'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [],
+    // 'allowed_origins_patterns' => [],
 
-    // 'allowed_origins_patterns' => ['/([\w.:]*)(google.com)/i'],
+    'allowed_origins_patterns' => ['/([\w.:]*)(google.com)/i'],
 
     'allowed_headers' => ['*'],
 
@@ -33,5 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
+    // 'supports_credentials' => false,
 ];
