ceb38174f4c6cb53af518c610ac524c6d0a127a5
Hamza Aslam <imhamza@outlook.com>
2023-04-12 15:38:41 +0500

    add edit and delete option for coupon

--- resources/views/spectator/coupon/index.blade.php
+++ resources/views/spectator/coupon/index.blade.php
@@ -8,12 +8,12 @@
                 <div class="card">
                     <div class="card-header">Coupons</div>
                     <div class="card-body">
-                        {{-- <div class="row ml-0 mr-0">
+                        <div class="row ml-0 mr-0">
                             <div class="col-12 text-right">
                                 <a href="{{route('spectator.coupon.create')}}" class="btn btn-primary my-3">Add Coupon</a>
 
                             </div>
-                        </div> --}}
+                        </div>
                         <table aria-label="Coupons" class="table table-hoved table-bordered">
                             <thead>
                             <th scope="col">Id</th>
@@ -22,7 +22,7 @@
                             <th scope="col">Discount Percent</th>
                             <th scope="col">Recurring Count</th>
                             <th scope="col">Expire At</th>
-                            {{-- <th scope="col">Actions</th> --}}
+                            <th scope="col">Actions</th>
                             </thead>
                             <tbody>
                             @forelse($coupons as $coupon)
@@ -33,7 +33,7 @@
                                     <td>{{$coupon->discount_percent}}</td>
                                     <td>{{$coupon->recurring_discount_count}}</td>
                                     <td>{{$coupon->expires_at->todateString()}}</td>
-                                    {{-- <td class="d-flex flex-row">
+                                    <td class="d-flex flex-row">
                                         <a href="{{route('spectator.coupon.edit',$coupon->id)}}" class="btn btn-primary m-2">Edit</a>
                                         <form action="{{route('spectator.coupon.destroy',$coupon->id)}}" method="post" onsubmit="event.preventDefault(); if(confirm('Do you really want to delete this entity?')) this.submit();">
                                             @csrf
@@ -42,7 +42,7 @@
 
                                         </form>
 
-                                    </td> --}}
+                                    </td>
 
                                 </tr>
                             @empty
