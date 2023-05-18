b55fcb373f32bde8158585311145bccf2a409556
Shahroz Rajpoot <m.shahroz6201@gmail.com>
2023-05-18 14:09:15 +0500

    Fixed delete country error

--- resources/js/react/UserInterface/utils/AddKeyword.js
+++ resources/js/react/UserInterface/utils/AddKeyword.js
@@ -200,17 +200,20 @@ export default class AddKeyword extends React.Component {
     }
 
     changeLocationHandler(selectedLocations) {
+
+        const selectedLocationsLength = selectedLocations?.length ? selectedLocations.length : 0
+
         if (
             this.canAddMoreConfigurations(
                 this.state.keywords.length,
-                selectedLocations.length,
+                selectedLocationsLength,
                 this.state.search_engines.length
             )
         ) {
             this.setState({
                 locations: selectedLocations,
             });
-            this.updateAvailableCredits(this.state.keywords.length, selectedLocations.length, this.state.search_engines.length);
+            this.updateAvailableCredits(this.state.keywords.length, selectedLocationsLength, this.state.search_engines.length);
             return true;
         } else {
             this.props.upgradePopupForRankingTracking();
