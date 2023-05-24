94991aa999f502cb47b493ca549709103af31e9c
Shahroz <m.shahroz6201@gmial.com>
2023-05-24 15:00:45 +0500

    Updated google algorithm updates

--- app/Helpers/AnnotationQueryHelper.php
+++ app/Helpers/AnnotationQueryHelper.php
@@ -179,18 +179,23 @@ class AnnotationQueryHelper
     public static function googleAlgorithmQuery(array $userIdsArray)
     {
         $annotationsQuery = "";
-        $annotationsQuery .= "select 1, update_date AS show_at, NULL, category, event_name, NULL as url, 
-        CONCAT('google_algorithm_updates', '~~~~', `google_algorithm_updates`.`id`,  '~~~~', 'System', '~~~~', 'System') AS `added_by`, description, 'System' AS `user_name`, update_date, `uds`.`ga_property_id` AS `table_ga_property_id` 
-        from `google_algorithm_updates` 
-        LEFT JOIN `user_data_sources` AS uds ON `uds`.`ds_code` = 'google_algorithm_update_dates' 
-        AND `uds`.`user_id` IN ('" . implode("', '", $userIdsArray) . "') ";
+        $annotationsQuery .= "SELECT 1, update_date AS show_at, NULL, category, event_name, NULL as url, CONCAT('google_algorithm_updates', '~~~~', `google_algorithm_updates`.`id`,  '~~~~', 'System', '~~~~', 'System') AS `added_by`, description, 'System' AS `user_name`, update_date, `uds`.`ga_property_id` AS `table_ga_property_id` FROM `google_algorithm_updates`, `user_data_sources` AS uds WHERE `uds`.`ds_code` = 'google_algorithm_update_dates' AND `uds`.`user_id` IN ('" . implode("', '", $userIdsArray) . "') ";
 
-        // $gAUConf = UserDataSource::whereIn('user_id', $userIdsArray)->where('ds_code', 'google_algorithm_update_dates')->first();
-        // if ($gAUConf) {
-        //     if ($gAUConf->status != '' && $gAUConf->status != null) {
-        //         $annotationsQuery .= ' where google_algorithm_updates.status = "' . $gAUConf->status . '"';
-        //     }
-        // }
+        $gAUConfs = UserDataSource::whereIn('user_id', $userIdsArray)->where('ds_code', 'google_algorithm_update_dates')->get();
+
+        if ($gAUConfs) {
+            $statusConstraints = [];
+            foreach ($gAUConfs as $gAUConf) {
+                if ($gAUConf->status != '' && $gAUConf->status != null) {
+                    array_push($statusConstraints, 'google_algorithm_updates.status = "' . $gAUConf->status . '"');
+                } else {
+                    array_push($statusConstraints, '(google_algorithm_updates.status = "confirmed" OR google_algorithm_updates.status = "unconfirmed")');
+                }
+            }
+            if (!empty($statusConstraints)) {
+                $annotationsQuery .= ' AND ' . implode(' OR ', $statusConstraints);
+            }
+        }
 
         return $annotationsQuery;
     }
