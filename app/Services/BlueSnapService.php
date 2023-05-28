567112ba8881584f84e01f29d8196ea11b5cef92
Hamza Aslam <imhamza@outlook.com>
2023-05-28 16:41:09 +0500

    Revert "Added 3DS in bluesnap token URL"
    This reverts commit 5b5d99993603ce09bed57ae2d73012d71eb654d1.

--- app/Services/BlueSnapService.php
+++ app/Services/BlueSnapService.php
@@ -28,7 +28,7 @@ class BlueSnapService
     public function getToken()
     {
 
-        $url = $this->blueSnapDomain . '/services/2/payment-fields-tokens?challengerequested3ds=true';
+        $url = $this->blueSnapDomain . '/services/2/payment-fields-tokens';
 
         $response = Http::withBasicAuth($this->key, $this->password)
             ->post($url);
